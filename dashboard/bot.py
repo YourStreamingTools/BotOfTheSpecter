65d20d4fa7a5445f76fb3d5580ee8750f22733f8
GFAUnDead <contact@gfaundead.stream>
2023-10-18 10:43:19 +1100

    Update bot.py

--- dashboard/bot.py
+++ dashboard/bot.py
@@ -1,6 +1,8 @@
 import os
+import asyncio
 import twitchio
-from twitchio.ext import commands, pubsub
+from twitchio.ext import commands
+from twitchio.ext import pubsub
 import sqlite3
 import time
 import argparse
@@ -13,7 +15,7 @@ import signal
 parser = argparse.ArgumentParser(description="BotOfTheSpecter Chat Bot")
 parser.add_argument("-channel", dest="target_channel", required=True, help="Target Twitch channel name")
 parser.add_argument("-channelid", dest="channel_id", required=True, help="Twitch user ID")
-parser.add_argument("-token", dest="auth_token", required=True, help="Auth Token for authentication")
+parser.add_argument("-token", dest="channel_auth_token", required=True, help="Auth Token for authentication")
 args = parser.parse_args()
 
 # Define a signal handler function to handle Ctrl+C (SIGINT)
@@ -31,10 +33,15 @@ OAUTH_TOKEN = ""  # CHANGE TO MAKE THIS WORK
 CLIENT_ID = ""    # CHANGE TO MAKE THIS WORK
 CHANNEL_NAME = args.target_channel
 CHANNEL_ID = args.channel_id
+CHANNEL_AUTH = args.channel_auth_token
 
-# Initialize the bot
-bot = commands.Bot(token=OAUTH_TOKEN, prefix='!', initial_channels=[CHANNEL_NAME])
-pubsub_client = pubsub.PubSub(client_id=CLIENT_ID)
+# Create the bot instance
+bot = commands.Bot(
+    token=OAUTH_TOKEN,
+    prefix='!',
+    initial_channels=[CHANNEL_NAME],
+    nick=BOT_USERNAME,
+)
 
 # Logs
 webroot = "/var/www/html"
@@ -51,49 +58,43 @@ for directory in [logs_directory, bot_logs]:
 log_file = os.path.join(webroot, bot_logs, f"{CHANNEL_NAME}.txt")
 logging.basicConfig(filename=log_file, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-class Bot(commands.Bot):
-    def __init__(self, cursor):
-        super().__init__(token=OAUTH_TOKEN, prefix='!', initial_channels=[CHANNEL_NAME])
-        self.cursor = cursor
+# Create an instance of your Bot class
+bot_instance = bot
 
-    async def event_ready(self):
-        logging.info(f'Logged in as | {self.nick}')
-        logging.info(f'User id is | {self.user_id}')
-        await pubsub_client.pubsub_channel_subscribe(CLIENT_ID, f'channel.{CHANNEL_NAME}')
+# Define the pubsub_client outside the class
+pubsub_client = pubsub.PubSubPool(bot_instance)
 
-    @commands.command()
-    async def start_bot(self, ctx: commands.Context):
-        requests_made = 0  # Initialize requests_made
-        start_time = time.time()  # Initialize start_time
-        while True:
-            current_time = int(time.time())  # Get current UNIX timestamp
-            # Configure logging here if needed
+@bot.event
+async def event_ready():
+    logging.info(f'Logged in as | {bot_instance.nick}')
+    logging.info(f'User id is | {bot_instance.user_id}')
+    await pubsub_client.pubsub_channel_subscribe(CLIENT_ID, f'channel.{CHANNEL_NAME}')
 
-@pubsub_client.event
+@commands.command()
+async def start_bot(self, ctx: commands.Context):
+    requests_made = 0  # Initialize requests_made
+    start_time = time.time()  # Initialize start_time
+    while True:
+        current_time = int(time.time())  # Get current UNIX timestamp
+        # Configure logging here if needed
+
+@bot.event()
 async def on_pubsub_channel_subscription(data):
     if data['type'] == 'stream.online':
         print(f"The stream is now online, {BOT_USERNAME} is ready!")
 
-@bot.event
+@bot.event()
 async def event_new_follower(follower):
     print(f"New follower: {follower.name}")
 
-@bot.event
+@bot.event()
 async def event_cheer(cheerer, message):
     print(f"{cheerer.display_name} cheered {message.bits} bits!")
 
-@bot.event
+    
+@bot.event()
 async def event_subscribe(subscriber):
     print(f"{subscriber.display_name} just subscribed to the channel!")
 
-# Create a connection to the SQLite database
-database_folder = "database"
-database_file = os.path.join(database_folder, f"{CHANNEL_NAME.lower()}.db")
-conn = sqlite3.connect(database_file)
-cursor = conn.cursor()
-
-# Create an instance of your Bot class and pass the cursor
-bot_instance = Bot(cursor)
-
-# Run the bot instance
-bot_instance.run()
+# Run the bot
+bot.run()
