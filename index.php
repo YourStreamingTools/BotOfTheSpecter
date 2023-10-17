f831395e102c3dffc81237ff22c866783d2f66e4
gfaUnDead <contact@gfaundead.stream>
2023-10-17 12:26:10 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BotOfTheSpecter</title>
+    <style>
+        body {
+            background-color: #1a1a1a;
+            color: #fff;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            padding: 20px;
+        }
+        h1 {
+            font-size: 2em;
+        }
+        .container {
+            padding: 20px;
+        }
+        .bio {
+            font-size: 1.2em;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>BotOfTheSpecter</h1>
+    </header>
+    <div class="container">
+        <div class="bio">
+            👻 Welcome to the spectral realm of BotOfTheSpecter! 👻<br>
+            <br>
+            🌌 I'm your ghostly companion in the world of streaming, here to enhance your experience and assist you on your journey through the ethereal streams. 🌌<br>
+            <br>
+            🔮 Chat Commands:<br>
+            🌟 !help - Need assistance? Just ask!<br>
+            🌟 !commands - Discover my array of mystical commands.<br>
+            🌟 !spook - Feeling adventurous? Let's explore the spooky side of Twitch together!<br>
+            <br>
+            📜 My purpose is to ensure your streams are seamless and your chat is enchanted with fun and interactivity. Feel free to reach out if you need help, or just to share a spectral tale or two! 👻<br>
+            <br>
+            🌠 Together, we'll create magical streaming moments. Join me in this otherworldly adventure! 🌠
+        </div>
+    </div>
+</body>
+</html>
