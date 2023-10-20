d86c93189449031a98b250b70e073bf93093c23e
GFAUnDead <contact@gfaundead.stream>
2023-10-20 15:20:46 +1100

    Update vips.php

--- dashboard/vips.php
+++ dashboard/vips.php
@@ -129,6 +129,7 @@ $displaySearchBar = count($allVIPs) > $vipsPerPage;
       <li><a href="followers.php">View Followers</a></li>
       <li><a href="subscribers.php">View Subscribers</a></li>
       <li class="is-active"><a href="vips.php">View VIPs</a></li>
+      <li><a href="logs.php">View Logs</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
