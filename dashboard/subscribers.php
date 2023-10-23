5918e9fe8aad0e013f31adab7754463e64e2cf30
GFAUnDead <contact@gfaundead.stream>
2023-10-23 14:37:23 +1100

    Update subscribers.php

--- dashboard/subscribers.php
+++ dashboard/subscribers.php
@@ -130,6 +130,7 @@ $displaySearchBar = count($allSubscribers) > $subscribersPerPage;
       <li class="is-active"><a href="subscribers.php">View Subscribers</a></li>
       <li><a href="vips.php">View VIPs</a></li>
       <li><a href="logs.php">View Logs</a></li>
+      <li><a href="commands.php">Bot Commands</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
