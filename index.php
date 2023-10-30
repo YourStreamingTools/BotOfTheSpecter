679c4f7493d6e812bd3b3beceed6482f57245ab3
gfaUnDead <contact@gfaundead.stream>
2023-10-30 16:16:58 +1100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BotOfTheSpecter</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+  	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
+  	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
 </head>
 <body>
     <header>
