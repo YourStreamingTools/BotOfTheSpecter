f02d9fee5085d5c47535df83dd71a10c8fce32b4
GFAUnDead <contact@gfaundead.stream>
2023-10-30 12:58:53 +1100

    Update commands.php

--- dashboard/commands.php
+++ dashboard/commands.php
@@ -83,6 +83,7 @@ try {
       <li><a href="vips.php">View VIPs</a></li>
       <li><a href="logs.php">View Logs</a></li>
       <li class="is-active"><a href="commands.php">Bot Commands</a></li>
+      <li><a href="add-commands.php">Add Bot Command</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
