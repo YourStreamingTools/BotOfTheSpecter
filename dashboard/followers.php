5310f49d71315ecfd145be4f2df20dd77166b190
GFAUnDead <contact@gfaundead.stream>
2023-10-30 12:58:57 +1100

    Update followers.php

--- dashboard/followers.php
+++ dashboard/followers.php
@@ -164,6 +164,7 @@ $displaySearchBar = count($allFollowers) > $followersPerPage;
       <li><a href="vips.php">View VIPs</a></li>
       <li><a href="logs.php">View Logs</a></li>
       <li><a href="commands.php">Bot Commands</a></li>
+      <li><a href="add-commands.php">Add Bot Command</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
