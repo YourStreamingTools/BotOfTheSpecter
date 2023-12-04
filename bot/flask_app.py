3a5170a67e8931bd753212147f35b130206f651f
GFAUnDead <contact@gfaundead.stream>
2023-12-04 12:28:05 +1100

    Create flask_app.py

---
+++ bot/flask_app.py
@@ -0,0 +1,17 @@
+import flask
+from flask import Flask, request
+import argparse
+import asyncio
+import os
+import sqlite3
+
+app = Flask("TwitchWebhookServer")
+
+def start_app(port):
+    app.run(port=port)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Start the Flask app")
+    parser.add_argument("-port", type=int, default=5000, help="Port for the Flask app")
+    args = parser.parse_args()
+    start_app(args.port)
