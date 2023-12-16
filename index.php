5cd9017f71962f8fb527f80c341a03d45eeedced
gfaUnDead <contact@gfaundead.stream>
2023-12-16 14:15:26 +1100

    Update index.php

--- index.php
+++ index.php
@@ -15,16 +15,11 @@
     </header>
     <div class="container">
         <div class="bio">
-            <p><span class="icon">&#128123;</span> Welcome to the spectral realm of BotOfTheSpecter! <span class="icon">&#128123;</span></p>
-            <p><span class="icon">&#127757;</span> I'm your ghostly companion in the world of streaming, here to enhance your experience and assist you on your journey through the ethereal streams. <span class="icon">&#127757;</span></p>
-            <p><span class="icon">&#128302;</span> Chat Commands:</p>
-                <ul>
-                    <li><span class="icon">&#127775;</span> !help - Need assistance? Just ask!</li>
-                    <li><span class="icon">&#127775;</span> !commands - Discover my array of mystical commands.</li>
-                    <li><span class="icon">&#127775;</span> !spook - Feeling adventurous? Let's explore the spooky side of Twitch together!</li>
-                </ul>
-            <p><span class="icon">&#128220;</span> My purpose is to ensure your streams are seamless and your chat is enchanted with fun and interactivity. Feel free to reach out if you need help, or just to share a spectral tale or two! <span class="icon">&#128123;</span></p>
-            <p><span class="icon">&#127776;</span> Together, we'll create magical streaming moments. I'll join you in this adventure! <span class="icon">&#127776;</span></p>
+            <p><span class="icon">&#127757;</span> I'm BotOfTheSpecter, your ghostly guardian in the ever-expanding universe of streaming. <span class="icon">&#128123;</span></p>
+            <p><span class="icon">&#127775;</span> !commands - Unleash the full potential of my enchanted command list.</p>
+            <p><span class="icon">&#128220;</span> Your Ghostly Streaming Companion: My essence is woven into the fabric of your streams, ensuring smooth sailing through the digital waves. <span class="icon">&#128123;</span></p>
+            <p><span class="icon">&#127770;</span> Join the Spectral Streaming Society: Step into the light of the streaming moon and let's weave some digital magic. <span class="icon">&#127770;</span></p>
+            <p><span class="icon">&#128123;</span> BotOfTheSpecter: Where Streaming Meets the Supernatural. <span class="icon">&#128123;</span></p>
         </div>
     </div>
     <footer>
