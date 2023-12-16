f5d75e6a2b3d71a275f12d02e545e7db19dfb5f4
gfaUnDead <contact@gfaundead.stream>
2023-12-16 19:31:33 +1100

    Update vips.php

--- dashboard/vips.php
+++ dashboard/vips.php
@@ -133,6 +133,7 @@ $displaySearchBar = count($allVIPs) > $vipsPerPage;
       <li><a href="logs.php">View Logs</a></li>
       <li><a href="commands.php">Bot Commands</a></li>
       <li><a href="add-commands.php">Add Bot Command</a></li>
+      <li><a href="edit_typos.php">Edit Typos</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
