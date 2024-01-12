950101e85e05df70f4e4e289ced7f1bfc12c5546
gfaUnDead <contact@gfaundead.stream>
2024-01-13 03:10:03 +1100

    Update bot.php

--- dashboard/bot.php
+++ dashboard/bot.php
@@ -132,7 +132,7 @@ if ($ModStatusOutput) {
 
 <div class="row column">
 <br>
-<h1><?php echo "$greeting, <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>$twitchDisplayName!"; ?></h1>
+<h1><?php echo "$greeting, $twitchDisplayName <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
 <br>
 <?php echo $BotModMessage; ?>
 <h3><?php echo $statusOutput; ?></h3>
