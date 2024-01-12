1811a820bceb9486547707a3d1f789ebef5bc87f
gfaUnDead <contact@gfaundead.stream>
2024-01-13 03:11:09 +1100

    Update subscribers.php

--- dashboard/subscribers.php
+++ dashboard/subscribers.php
@@ -140,7 +140,7 @@ $displaySearchBar = count($allSubscribers) > $subscribersPerPage;
 
 <div class="row column">
   <br>
-  <h1><?php echo "$greeting, <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>$twitchDisplayName!"; ?></h1>
+  <h1><?php echo "$greeting, $twitchDisplayName <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
   <br>
   <?php if ($displaySearchBar) : ?>
     <div class="row column">
