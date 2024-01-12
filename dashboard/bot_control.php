041c7162708f7c1e7ef2d336c417348256247c18
gfaUnDead <contact@gfaundead.stream>
2024-01-13 01:38:33 +1100

    Update bot_control.php

--- dashboard/bot_control.php
+++ dashboard/bot_control.php
@@ -1,3 +1,4 @@
+<?php ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); ?>
 <?php
 $botScriptPath = "/var/www/bot/bot.py";
 $statusScriptPath = "/var/www/bot/status.py";
