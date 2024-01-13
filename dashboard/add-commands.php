df565905887494c21918a5271b5c9b3353e8c02f
gfaUnDead <contact@gfaundead.stream>
2024-01-13 22:04:27 +1100

    Update add-commands.php

--- dashboard/add-commands.php
+++ dashboard/add-commands.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
     <link rel="stylesheet" href="pagination.css">
-    <script src="https://cdn.yourstreaming.tools/js/about.js"></script>
+    <script src="about.js"></script>
   	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
   	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <!-- <?php echo "User: $username | $twitchUserId | $authToken"; ?> -->
