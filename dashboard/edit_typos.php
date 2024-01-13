63ec0975a55d08782989d17c26fa1d9693b1b4d6
gfaUnDead <contact@gfaundead.stream>
2024-01-13 22:04:46 +1100

    Update edit_typos.php

--- dashboard/edit_typos.php
+++ dashboard/edit_typos.php
@@ -111,7 +111,7 @@ $typoCountsJs = json_encode(array_column($typoData, 'typo_count', 'username'));
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
     <link rel="stylesheet" href="pagination.css">
-    <script src="https://cdn.yourstreaming.tools/js/about.js"></script>
+    <script src="about.js"></script>
   	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
   	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <script>var typoCounts = <?php echo $typoCountsJs ?></script>
