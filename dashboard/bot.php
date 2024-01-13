e1899e3f2461a4cd933bdd0fb8043be3d725c064
gfaUnDead <contact@gfaundead.stream>
2024-01-13 22:04:31 +1100

    Update bot.php

--- dashboard/bot.php
+++ dashboard/bot.php
@@ -89,7 +89,7 @@ if ($ModStatusOutput) {
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
     <link rel="stylesheet" href="pagination.css">
-    <script src="https://cdn.yourstreaming.tools/js/about.js"></script>
+    <script src="about.js"></script>
   	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
   	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <!-- <?php echo "User: $username | $twitchUserId | $authToken | Port: $webhookPort"; ?> -->
