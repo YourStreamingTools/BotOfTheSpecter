60f4c754482af169957c27fa340a526ac68486dc
gfaUnDead <contact@gfaundead.stream>
2024-01-14 12:16:40 +1100

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,10 @@
+import twitch_auth
+
+def main():
+    # Start the Twitch login process
+    twitch_auth.login()
+
+    # Code for after successful login place here
+
+if __name__ == "__main__":
+    main()
