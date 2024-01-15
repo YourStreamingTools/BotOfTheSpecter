4c97ed8e26d9000c8efd68640b87093117410fe3
gfaUnDead <contact@gfaundead.stream>
2024-01-15 20:10:35 +1100

    Update twitch_auth.py

--- app/twitch_auth.py
+++ app/twitch_auth.py
@@ -8,6 +8,7 @@ CLIENT_ID = "" # CHANGE TO MAKE THIS WORK
 CLIENT_SECRET = "" # CHANGE TO MAKE THIS WORK
 REDIRECT_URI = "http://localhost:5000/auth"
 AUTH_URL = f"https://id.twitch.tv/oauth2/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=user:read:email"
+global_username = None
 
 class AuthHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -30,7 +31,21 @@ def exchange_code_for_token(code):
     }
     response = requests.post("https://id.twitch.tv/oauth2/token", data=payload)
     access_token = response.json().get('access_token')
-    # Here, you should handle the access_token (store it, use it, etc.)
+    
+    # Use the access token to get the user's information
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Client-ID': CLIENT_ID
+    }
+    user_response = requests.get("https://api.twitch.tv/helix/users", headers=headers)
+    user_info = user_response.json().get('data', [{}])[0]
+    username = user_info.get('login')
+
+    global global_username
+    global_username = username
+
+    # Print the username to the console
+    print(f"Authenticated Twitch user: {username}")
 
 def run_server():
     server_address = ('', 5000)
@@ -40,6 +55,3 @@ def run_server():
 def start_auth():
     threading.Thread(target=run_server, daemon=True).start()
     webbrowser.open(AUTH_URL)
-
-if __name__ == '__main__':
-    start_auth()
