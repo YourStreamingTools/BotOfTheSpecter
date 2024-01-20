502e729f3055bfa8359cc40b36be10c62e1ba853
gfaUnDead <contact@gfaundead.stream>
2024-01-20 17:09:40 +1100

    Update main.py
    Moved a lot of the server communication to the server_communication.py file.

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ import os
 import tkinter as tk
 from tkinter import ttk, messagebox
 import requests
+import webbrowser
 import threading
-import server_communication
+from server_communication import is_user_authorized, check_bot_status, \
+                            run_bot, stop_bot, restart_bot, fetch_and_show_logs
 import twitch_auth
 import logging
 import paramiko
-import webbrowser
 from decouple import config
 
 threading.Thread(target=twitch_auth.start_auth, daemon=True).start()
@@ -17,96 +18,8 @@ os.makedirs(appdata_dir, exist_ok=True)
 log_file_path = os.path.join(appdata_dir, 'main.log')
 
 # Get variables
-REMOTE_SSH_HOST="" # CHANGE TO MAKE THIS WORK
-REMOTE_SSH_PORT="" # CHANGE TO MAKE THIS WORK
-REMOTE_SSH_USERNAME="" # CHANGE TO MAKE THIS WORK
-REMOTE_SSH_PASSWORD="" # CHANGE TO MAKE THIS WORK
-REMOTE_COMMAND_TEMPLATE="" # CHANGE TO MAKE THIS WORK
 REMOTE_VERSION_URL="https://api.botofthespecter.com/version_control.txt"
 VERSION="1.6.1"
-
-# Function to run the bot
-def run_bot():
-    status_label.config(text="Bot can't be started form this app.")
-
-# Function to check bot status
-def check_bot_status():
-    display_name = get_global_display_name()
-    username = get_global_username()
-
-    if not display_name:
-        status_label.config(text="User is not authenticated.")
-        return
-
-    if not server_communication.is_user_authorized(display_name):
-        status_label.config(text=f"{display_name} is not authorized to access this application.")
-        return
-
-    ssh_host = REMOTE_SSH_HOST
-    ssh_port = REMOTE_SSH_PORT
-    ssh_username = REMOTE_SSH_USERNAME
-    ssh_password = REMOTE_SSH_PASSWORD
-    remote_command_template = REMOTE_COMMAND_TEMPLATE
-
-    remote_command = f"{remote_command_template} -channel {username}"
-
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        ssh.connect(ssh_host, int(ssh_port), ssh_username, ssh_password)
-
-        stdin, stdout, stderr = ssh.exec_command(remote_command)
-
-        output = stdout.read().decode("utf-8")
-        error = stderr.read().decode("utf-8")
-
-        if error:
-            status_label.config(text=f"Error: {error}")
-        else:
-            status_label.config(text=output)
-
-        ssh.close()
-    except Exception as e:
-        status_label.config(text=f"Error: {str(e)}")
-
-# Function to stop the bot
-def stop_bot():
-    status_label.config(text="Bot can't be stopped form this app.")
-
-# Function to restart the bot
-def restart_bot():
-    status_label.config(text="Bot can't be restarted form this app.")
-
-def get_global_username():
-    return twitch_auth.global_username
-def get_global_display_name():
-    return twitch_auth.global_display_name
-
-def fetch_and_show_logs(log_type):
-    display_name = get_global_display_name()
-
-    if not display_name:
-        text_area.delete(1.0, tk.END)
-        text_area.insert(tk.END, "User is not authenticated.")
-        return
-
-    if not server_communication.is_user_authorized(display_name):
-        text_area.delete(1.0, tk.END)
-        text_area.insert(tk.END, f"{display_name} is not authorized to access this application.")
-        return
-
-    if log_type == 'bot':
-        logs = server_communication.get_bot_logs()
-    elif log_type == 'chat':
-        logs = server_communication.get_chat_logs()
-    elif log_type == 'twitch':
-        logs = server_communication.get_twitch_logs()
-    else:
-        logs = "Invalid log type"
-
-    text_area.delete(1.0, tk.END)
-    text_area.insert(tk.END, logs)
     
 # Function to check for updates
 def custom_messagebox(title, message, buttons):
