5d4b6c6f2d8eab2fda6a1a009586d7f1da39ddff
GFAUnDead <contact@gfaundead.stream>
2024-02-13 10:36:01 +1100

    Update mods.php

--- dashboard/mods.php
+++ dashboard/mods.php
@@ -132,7 +132,7 @@ $moderatorsForCurrentPage = array_slice($allModerators, $startIndex, $moderators
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 BotOfTheSpecter. All rights reserved.</a></li>
+      <li><a class="popup-link" onclick="showPopup()">&copy; 2023-<?php echo date("Y"); ?> BotOfTheSpecter. All rights reserved.</a></li>
     </ul>
   </div>
 </nav>
