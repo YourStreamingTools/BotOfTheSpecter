647c194f7bfc9649054a380194dd5d198ce34d84
gfaUnDead <contact@gfaundead.stream>
2024-03-07 17:43:55 +1100

    Update profile.js

--- dashboard/profile.js
+++ dashboard/profile.js
@@ -1,18 +1,12 @@
 $(document).ready(function() {
   $("#show-api-key").click(function() {
-    console.log("Show API Key button clicked");
-    console.log($(".api-key-wrapper").css("display"));
     $(".api-key-wrapper").show();
-    console.log($(".api-key-wrapper").css("display"));
     $("#show-api-key").hide();
     $("#hide-api-key").show();
   });
 
   $("#hide-api-key").click(function() {
-    console.log("Hide API Key button clicked");
-    console.log($(".api-key-wrapper").css("display"));
     $(".api-key-wrapper").hide();
-    console.log($(".api-key-wrapper").css("display"));
     $("#show-api-key").show();
     $("#hide-api-key").hide();
   });
