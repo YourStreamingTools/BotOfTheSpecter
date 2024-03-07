a49a2cd66ff1d262bf96412ec2f3fffb1fcab51e
gfaUnDead <contact@gfaundead.stream>
2024-03-07 17:47:40 +1100

    Update bot.php

--- dashboard/bot.php
+++ dashboard/bot.php
@@ -122,6 +122,7 @@ if ($ModStatusOutput) {
       <li><a href="add-commands.php">Add Bot Command</a></li>
       <li><a href="edit_typos.php">Edit Typos</a></li>
       <li><a href="app.php">Download App</a></li>
+      <li><a href="profile.php">Profile</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
