3cfc1b6a82dd44f35fd7845bdc7f0d2a62dab98f
gfaUnDead <contact@gfaundead.stream>
2024-03-07 17:46:30 +1100

    Update counters.php

--- dashboard/counters.php
+++ dashboard/counters.php
@@ -72,6 +72,7 @@ $countType= '';
       <li><a href="add-commands.php">Add Bot Command</a></li>
       <li><a href="edit_typos.php">Edit Typos</a></li>
       <li><a href="app.php">Download App</a></li>
+      <li><a href="profile.php">Profile</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
