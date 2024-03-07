77c54b321debda27697b2f9f63237bcf094ab854
gfaUnDead <contact@gfaundead.stream>
2024-03-07 17:46:35 +1100

    Update edit_typos.php

--- dashboard/edit_typos.php
+++ dashboard/edit_typos.php
@@ -143,6 +143,7 @@ $typoCountsJs = json_encode(array_column($typoData, 'typo_count', 'username'));
       <li><a href="add-commands.php">Add Bot Command</a></li>
       <li class="is-active"><a href="edit_typos.php">Edit Typos</a></li>
       <li><a href="app.php">Download App</a></li>
+      <li><a href="profile.php">Profile</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
