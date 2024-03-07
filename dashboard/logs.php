ce8d8fef9b5c66474f859fee7211947235599310
gfaUnDead <contact@gfaundead.stream>
2024-03-07 17:46:48 +1100

    Update logs.php

--- dashboard/logs.php
+++ dashboard/logs.php
@@ -88,6 +88,7 @@ if(isset($_GET['logType'])) {
       <li><a href="add-commands.php">Add Bot Command</a></li>
       <li><a href="edit_typos.php">Edit Typos</a></li>
       <li><a href="app.php">Download App</a></li>
+      <li><a href="profile.php">Profile</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
