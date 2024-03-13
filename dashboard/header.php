529d089b15628f1db4eb58736f5f6f6401516b5c
gfaUnDead <contact@gfaundead.stream>
2024-03-13 20:33:51 +1100

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -6,24 +6,24 @@
   <div class="top-bar-left">
     <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
       <li class="menu-text">BotOfTheSpecter</li>
-      <li><a href="bot.php">Dashboard</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/bot.php') echo 'class="is-active"'; ?>><a href="bot.php">Dashboard</a></li>
       <li>
-        <a>Twitch Data</a>
+        <a <?php if(strpos($_SERVER['REQUEST_URI'], 'mods.php') !== false || strpos($_SERVER['REQUEST_URI'], 'followers.php') !== false || strpos($_SERVER['REQUEST_URI'], 'subscribers.php') !== false || strpos($_SERVER['REQUEST_URI'], 'vips.php') !== false) echo 'class="is-active"'; ?>>Twitch Data</a>
         <ul class="vertical menu" data-dropdown-menu>
-          <li><a href="mods.php">View Mods</a></li>
-          <li><a href="followers.php">View Followers</a></li>
-          <li><a href="subscribers.php">View Subscribers</a></li>
-          <li><a href="vips.php">View VIPs</a></li>
+          <li <?php if($_SERVER['REQUEST_URI'] == '/mods.php') echo 'class="is-active"'; ?>><a href="mods.php">View Mods</a></li>
+          <li <?php if($_SERVER['REQUEST_URI'] == '/followers.php') echo 'class="is-active"'; ?>><a href="followers.php">View Followers</a></li>
+          <li <?php if($_SERVER['REQUEST_URI'] == '/subscribers.php') echo 'class="is-active"'; ?>><a href="subscribers.php">View Subscribers</a></li>
+          <li <?php if($_SERVER['REQUEST_URI'] == '/vips.php') echo 'class="is-active"'; ?>><a href="vips.php">View VIPs</a></li>
         </ul>
       </li>
-      <li><a href="logs.php">View Logs</a></li>
-      <li><a href="counters.php">Counters</a></li>
-      <li><a href="commands.php">Bot Commands</a></li>
-      <li><a href="add-commands.php">Add Bot Command</a></li>
-      <li><a href="edit_typos.php">Edit Typos</a></li>
-      <li><a href="app.php">Download App</a></li>
-      <li><a href="profile.php">Profile</a></li>
-      <li><a href="logout.php">Logout</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/logs.php') echo 'class="is-active"'; ?>><a href="logs.php">View Logs</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/counters.php') echo 'class="is-active"'; ?>><a href="counters.php">Counters</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/commands.php') echo 'class="is-active"'; ?>><a href="commands.php">Bot Commands</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/add-commands.php') echo 'class="is-active"'; ?>><a href="add-commands.php">Add Bot Command</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/edit_typos.php') echo 'class="is-active"'; ?>><a href="edit_typos.php">Edit Typos</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/app.php') echo 'class="is-active"'; ?>><a href="app.php">Download App</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/profile.php') echo 'class="is-active"'; ?>><a href="profile.php">Profile</a></li>
+      <li <?php if($_SERVER['REQUEST_URI'] == '/logout.php') echo 'class="is-active"'; ?>><a href="logout.php">Logout</a></li>
     </ul>
   </div>
   <div class="top-bar-right">
