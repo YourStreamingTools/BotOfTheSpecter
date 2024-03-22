6eedfdf082c838949e76a19ff80a0e6f022d0640
GFAUnDead <contact@gfaundead.stream>
2024-03-22 12:56:12 +1100

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -18,8 +18,15 @@
       </li>
       <?php if($_SERVER['REQUEST_URI'] == '/logs.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="logs.php">View Logs</a></li>
       <?php if($_SERVER['REQUEST_URI'] == '/counters.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="counters.php">Counters</a></li>
-      <?php if($_SERVER['REQUEST_URI'] == '/commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="commands.php">Bot Commands</a></li>
-      <?php if($_SERVER['REQUEST_URI'] == '/add-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="add-commands.php">Add Bot Command</a></li>
+      <li>
+        <a>Bot Commnads</a>
+        <ul class="vertical menu" data-dropdown-menu>
+          <?php if($_SERVER['REQUEST_URI'] == '/commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="commands.php">View Custom Commands</a></li>
+          <?php if($_SERVER['REQUEST_URI'] == '/add-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="add-commands.php">Add Custom Command</a></li>
+          <?php if($_SERVER['REQUEST_URI'] == '/remove-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="">Remove Custom Command (COMING SOON)</a></li>
+          <?php if($_SERVER['REQUEST_URI'] == '/edit-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="">Edit Custom Command (COMING SOON)</a></li>
+        </ul>
+      </li>
       <?php if($_SERVER['REQUEST_URI'] == '/edit_typos.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="edit_typos.php">Edit Typos</a></li>
       <?php if($_SERVER['REQUEST_URI'] == '/app.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="app.php">Download App</a></li>
       <?php if($_SERVER['REQUEST_URI'] == '/profile.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="profile.php">Profile</a></li>
