0e702ce6027ddfd4cef6ba6fc1c1dea9bed96b99
gfaUnDead <contact@gfaundead.stream>
2024-03-23 23:37:53 +1100

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -25,7 +25,7 @@
           <?php if($_SERVER['REQUEST_URI'] == '/commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="commands.php">View Custom Commands</a></li>
           <?php if($_SERVER['REQUEST_URI'] == '/add-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="add-commands.php">Add Custom Command</a></li>
           <?php if($_SERVER['REQUEST_URI'] == '/remove-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="remove-commands.php">Remove Custom Command</a></li>
-          <?php if($_SERVER['REQUEST_URI'] == '/edit-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="">Edit Custom Command (COMING SOON)</a></li>
+          <?php if($_SERVER['REQUEST_URI'] == '/edit-commands.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="edit-commands.php">Edit Custom Command</a></li>
         </ul>
       </li>
       <?php if($_SERVER['REQUEST_URI'] == '/edit_typos.php') echo '<li class="is-active">'; else echo '<li>' ?><a href="edit_typos.php">Edit Typos</a></li>
