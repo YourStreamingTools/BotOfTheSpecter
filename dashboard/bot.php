a7f2cb475e010328de322f57905a150ad3b837b1
gfaUnDead <contact@gfaundead.stream>
2024-03-25 17:04:29 +1100

    Update bot.php

--- dashboard/bot.php
+++ dashboard/bot.php
@@ -9,6 +9,9 @@ if (!isset($_SESSION['access_token'])) {
     exit();
 }
 
+// Page Title
+$title = "Dashboard";
+
 // Connect to database
 require_once "db_connect.php";
 
@@ -85,21 +88,13 @@ if ($ModStatusOutput) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BotOfTheSpecter - Dashboard</title>
-    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
-    <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
-    <link rel="stylesheet" href="pagination.css">
-    <script src="about.js"></script>
-  	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-  	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
-    <!-- <?php echo "User: $username | $twitchUserId | $authToken | Webhook Port: $webhookPort | WebShocket Port: $websocketPort"; ?> -->
-	<!-- <?php echo "python $botScriptPath -channel $username -channelid $twitchUserId -token $authToken -refresh $refreshToken -hookport $webhookPort -socketport $websocketPort"; ?> -->
+    <!-- Headder -->
+    <?php include('headder.php'); ?>
+    <!-- /Headder -->
   </head>
 <body>
 <!-- Navigation -->
-<?php include('header.php'); ?>
+<?php include('navigation.php'); ?>
 <!-- /Navigation -->
 
 <div class="row column">
