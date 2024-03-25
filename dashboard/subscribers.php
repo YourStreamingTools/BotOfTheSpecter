b801d7a7e19e87de8134e83ba8127850a34bcfa4
gfaUnDead <contact@gfaundead.stream>
2024-03-25 17:50:59 +1100

    Update subscribers.php

--- dashboard/subscribers.php
+++ dashboard/subscribers.php
@@ -28,10 +28,15 @@ $broadcasterID = $user['twitch_user_id'];
 $twitchDisplayName = $user['twitch_display_name'];
 $twitch_profile_image_url = $user['profile_image'];
 $is_admin = ($user['is_admin'] == 1);
-$accessToken = $access_token;
+$authToken = $access_token;
+$refreshToken = $user['refresh_token'];
+$webhookPort = $user['webhook_port'];
+$websocketPort = $user['websocket_port'];
 $timezone = 'Australia/Sydney';
 date_default_timezone_set($timezone);
 $greeting = 'Hello';
+include 'bot_control.php';
+include 'sqlite.php';
 
 // API endpoint to fetch subscribers
 $subscribersURL = "https://api.twitch.tv/helix/subscriptions?broadcaster_id=$broadcasterID";
@@ -42,7 +47,7 @@ do {
     // Set up cURL request with headers
     $curl = curl_init($subscribersURL);
     curl_setopt($curl, CURLOPT_HTTPHEADER, [
-        'Authorization: Bearer ' . $accessToken,
+        'Authorization: Bearer ' . $authToken,
         'Client-ID: ' . $clientID
     ]);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -96,7 +101,7 @@ $displaySearchBar = count($allSubscribers) > $subscribersPerPage;
 <html lang="en">
   <head>
     <!-- Headder -->
-    <?php include('headder.php'); ?>
+    <?php include('header.php'); ?>
     <!-- /Headder -->
   </head>
 <body>
