949807cc1b04c27f7a8011fa189b117b9c22e151
gfaUnDead <contact@gfaundead.stream>
2024-03-25 17:17:10 +1100

    Update add-commands.php

--- dashboard/add-commands.php
+++ dashboard/add-commands.php
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en">
   <head>
     <!-- Headder -->
-    <?php include('headder.php'); ?>
+    <?php include('header.php'); ?>
     <!-- /Headder -->
   </head>
 <body>
