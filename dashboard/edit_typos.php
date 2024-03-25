06b8be7d43be45636512986271a1dba61226e0a4
gfaUnDead <contact@gfaundead.stream>
2024-03-25 17:02:07 +1100

    Update edit_typos.php

--- dashboard/edit_typos.php
+++ dashboard/edit_typos.php
@@ -12,6 +12,9 @@ if (!isset($_SESSION['access_token'])) {
     exit();
 }
 
+// Page Title
+$title = "Edit Typos";
+
 // Connect to database
 require_once "db_connect.php";
 
@@ -125,21 +128,14 @@ $typoCountsJs = json_encode(array_column($typoData, 'typo_count', 'username'));
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BotOfTheSpecter - Edit Typos</title>
-    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
-    <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
-    <link rel="stylesheet" href="pagination.css">
-    <script src="about.js"></script>
-    <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-    <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
-    <script>var typoCounts = <?php echo $typoCountsJs ?></script>
-</head>
+  <head>
+    <!-- Headder -->
+    <?php include('headder.php'); ?>
+    <!-- /Headder -->
+  </head>
 <body>
 <!-- Navigation -->
-<?php include('header.php'); ?>
+<?php include('navigation.php'); ?>
 <!-- /Navigation -->
 
 <div class="row column">
