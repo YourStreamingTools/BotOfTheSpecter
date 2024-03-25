075058a1668057bcca2de2ec16379d58687ff20f
gfaUnDead <contact@gfaundead.stream>
2024-03-25 17:04:43 +1100

    Update subscribers.php

--- dashboard/subscribers.php
+++ dashboard/subscribers.php
@@ -9,6 +9,9 @@ if (!isset($_SESSION['access_token'])) {
     exit();
 }
 
+// Page Title
+$title = "Twitch Subscribers";
+
 // Connect to database
 require_once "db_connect.php";
 
@@ -92,19 +95,13 @@ $displaySearchBar = count($allSubscribers) > $subscribersPerPage;
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BotOfTheSpecter - Twitch Subscribers</title>
-    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
-    <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
-    <link rel="stylesheet" href="pagination.css">
-    <script src="about.js"></script>
-    <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-    <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
+    <!-- Headder -->
+    <?php include('headder.php'); ?>
+    <!-- /Headder -->
   </head>
 <body>
 <!-- Navigation -->
-<?php include('header.php'); ?>
+<?php include('navigation.php'); ?>
 <!-- /Navigation -->
 
 <div class="row column">
