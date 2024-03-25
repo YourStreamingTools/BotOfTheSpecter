88704b2db1bf73ee478934551d87eade6d295b77
gfaUnDead <contact@gfaundead.stream>
2024-03-25 17:02:28 +1100

    Update profile.php

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -9,6 +9,9 @@ if (!isset($_SESSION['access_token'])) {
     exit();
 }
 
+// Page Title
+$title = "Profile";
+
 // Connect to database
 require_once "db_connect.php";
 
@@ -40,19 +43,13 @@ $last_login_utc = date_create_from_format('Y-m-d H:i:s', $last_login)->setTimezo
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BotOfTheSpecter - Dashboard</title>
-    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
-    <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
-    <link rel="stylesheet" href="pagination.css">
-    <script src="about.js"></script>
-  	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-  	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
+    <!-- Headder -->
+    <?php include('headder.php'); ?>
+    <!-- /Headder -->
   </head>
 <body>
 <!-- Navigation -->
-<?php include('header.php'); ?>
+<?php include('navigation.php'); ?>
 <!-- /Navigation -->
 
 <div class="row column">
