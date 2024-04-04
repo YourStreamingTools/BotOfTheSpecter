b648ece7bfa8a1ae8160706ad5ee33249ecef6e5
gfaUnDead <contact@gfaundead.stream>
2024-04-04 17:44:11 +1100

    Update commands.php

--- dashboard/commands.php
+++ dashboard/commands.php
@@ -87,6 +87,6 @@ include 'sqlite.php';
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
 <script>$(document).foundation();</script>
-<script src="search.js"></script>
+<script src="/js/search.js"></script>
 </body>
 </html>
