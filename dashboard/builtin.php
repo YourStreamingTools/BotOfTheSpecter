3f30432bb2c1ca7489425d817ae781c6fd6e8d9d
gfaUnDead <contact@gfaundead.stream>
2024-04-04 17:40:33 +1100

    Update builtin.php

--- dashboard/builtin.php
+++ dashboard/builtin.php
@@ -107,6 +107,6 @@ if ($result->num_rows > 0) {
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
 <script>$(document).foundation();</script>
-<script src="search.js"></script>
+<script src="/js/search.js"></script>
 </body>
 </html>
