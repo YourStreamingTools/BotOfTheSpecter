1a4eb28c733cba6443a3a922a86e914d05e9c446
gfaUnDead <contact@gfaundead.stream>
2024-04-22 20:12:10 +1000

    Create discordbot.php

---
+++ dashboard/discordbot.php
@@ -0,0 +1,65 @@
+<?php ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); ?>
+<?php
+// Initialize the session
+session_start();
+
+// check if user is logged in
+if (!isset($_SESSION['access_token'])) {
+    header('Location: login.php');
+    exit();
+}
+
+// Page Title
+$title = "Discord Bot";
+
+// Connect to database
+require_once "db_connect.php";
+
+// Fetch the user's data from the database based on the access_token
+$access_token = $_SESSION['access_token'];
+$userSTMT = $conn->prepare("SELECT * FROM users WHERE access_token = ?");
+$userSTMT->bind_param("s", $access_token);
+$userSTMT->execute();
+$userResult = $userSTMT->get_result();
+$user = $userResult->fetch_assoc();
+$user_id = $user['id'];
+$username = $user['username'];
+$twitchDisplayName = $user['twitch_display_name'];
+$twitch_profile_image_url = $user['profile_image'];
+$is_admin = ($user['is_admin'] == 1);
+$betaAccess = ($user['beta_access'] == 1);
+$twitchUserId = $user['twitch_user_id'];
+$broadcasterID = $twitchUserId;
+$authToken = $access_token;
+$refreshToken = $user['refresh_token'];
+$webhookPort = $user['webhook_port'];
+$websocketPort = $user['websocket_port'];
+$timezone = 'Australia/Sydney';
+date_default_timezone_set($timezone);
+$greeting = 'Hello';
+include 'sqlite.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Headder -->
+    <?php include('header.php'); ?>
+    <!-- /Headder -->
+  </head>
+<body>
+<!-- Navigation -->
+<?php include('navigation.php'); ?>
+<!-- /Navigation -->
+
+<div class="row column">
+<br>
+<h1><?php echo "$greeting, $twitchDisplayName <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
+<br>
+<h3>This page is coming soon!</h3>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>$(document).foundation();</script>
+</body>
+</html>
