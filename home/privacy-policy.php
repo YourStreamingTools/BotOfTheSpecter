b6a46a7d839da23beab598bf36ad4a9d489db17b
gfaUnDead <contact@gfaundead.stream>
2024-04-22 20:46:41 +1000

    Create privacy-policy.php

---
+++ home/privacy-policy.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BotOfTheSpecter - Privacy Policy</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
+  	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@Tools4Streaming" />
+    <meta name="twitter:title" content="BotOfTheSpecter" />
+    <meta name="twitter:description" content="BotOfTheSpecter is an advanced Twitch bot designed to enhance your streaming experience, offering a suite of tools for community interaction, channel management, and analytics." />
+    <meta name="twitter:image" content="https://cdn.botofthespecter.com/BotOfTheSpecter.jpeg" />
+</head>
+<body>
+    <header>
+        <h1>BotOfTheSpecter</h1>
+        <a href="https://botofthespecter.com/" class="login-button">HOME</a>
+    </header>
+    <div class="container">
+        <div class="bio">
+            <h1>Privacy Policy</h1>
+            <ol>
+                <li><strong>Information We Collect</strong>: We collect personal information you provide to us.</li>
+                <li><strong>How We Use Your Information</strong>: We use the information we collect to provide and improve our services.</li>
+                <li><strong>Sharing Your Information</strong>: We may share your information with third-party service providers.</li>
+                <li><strong>Data Retention</strong>: We retain your personal information for as long as necessary.</li>
+                <li><strong>Your Rights</strong>: You have the right to access, correct, or delete your personal information.</li>
+                <li><strong>Changes to Our Privacy Policy</strong>: We reserve the right to update or modify this Privacy Policy at any time.</li>
+                <li><strong>Contact Us</strong>: If you have any questions, please contact us at [contact@email.com].</li>
+            </ol>
+        </div>
+    </div>
+    <footer>
+        &copy; 2023-<?php echo date("Y"); ?> BotOfTheSpecter - All Rights Reserved. | Server 1
+    </footer>
+</body>
+</html>
