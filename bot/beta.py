c47e89ea296fe3749c7325009c4a5867ccc8e2e3
gfaUnDead <contact@gfaundead.stream>
2024-05-11 23:46:29 +1000

    Update beta.py

--- bot/beta.py
+++ bot/beta.py
@@ -3519,10 +3519,8 @@ async def process_giftsub_event(recipient_user_id, recipient_user_name, sub_plan
 async def process_followers_event(user_id, user_name, followed_at_twitch):
     # Truncate the timestamp to six digits for microseconds
     followed_at_twitch = followed_at_twitch[:26]
-
-    # Parse the truncated timestamp
-    datetime_obj = datetime.strptime(followed_at_twitch, "%Y-%m-%dT%H:%M:%S.%fZ")
-    followed_at = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+    time_now = datetime.now()
+    followed_at = time_now.strftime("%Y-%m-%d %H:%M:%S")
 
     # Insert a new record for the follower
     mysql_cursor.execute('INSERT INTO followers_data (user_id, user_name, followed_at) VALUES (%s, %s, %s)', (user_id, user_name, followed_at))
