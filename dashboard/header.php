2cb13464fbdebe4aab914821e2a3684cd64b061d
gfaUnDead <contact@gfaundead.stream>
2024-05-22 21:40:34 +1000

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -7,6 +7,7 @@
     <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
     <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/11.0.0/markdown-it.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <link rel="stylesheet" href="css/custom.css">
     <script src="/js/about.js"></script>
