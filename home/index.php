53817c1c013ce779beca4fde0c249a53b5a167ec
gfaUnDead <contact@gfaundead.stream>
2024-05-30 18:28:43 +1000

    Update index.php

--- home/index.php
+++ home/index.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BotOfTheSpecter</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-    <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
+  	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
+  	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@Tools4Streaming" />
     <meta name="twitter:title" content="BotOfTheSpecter" />
@@ -15,18 +14,14 @@
     <meta name="twitter:image" content="https://cdn.botofthespecter.com/BotOfTheSpecter.jpeg" />
 </head>
 <body>
-    <header class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">BotOfTheSpecter</h1>
-                <a href="https://dashboard.botofthespecter.com/" class="button is-light is-outlined">LOGIN</a>
-            </div>
-        </div>
+    <header>
+        <h1>BotOfTheSpecter</h1>
+        <a href="https://dashboard.botofthespecter.com/" class="login-button">LOGIN</a>
     </header>
-    <div class="container mt-6">
-        <div class="content">
+    <div class="container">
+        <div class="bio">
             <section>
-                <h2 class="title">Enhance Your Twitch and Discord Experience</h2>
+                <h2>Enhance Your Twitch and Discord Experience</h2>
                 <ul>
                     <li><strong>Interact with Your Community</strong>: Engage your viewers with custom chat commands on Twitch, and empower your members with interactive features on Discord.</li>
                     <li><strong>Stay Informed</strong>: Keep track of your Twitch followers, subscribers, and VIPs with ease, while also monitoring server activity and member engagement on Discord.</li>
@@ -34,17 +29,17 @@
                 </ul>
             </section>
             <section>
-                <h2 class="title">Tailored for Streamers and Server Owners</h2>
+                <h2>Tailored for Streamers and Server Owners</h2>
                 <p><strong>For Twitch Streamers</strong>: Manage your Twitch channel effortlessly with our intuitive dashboard and chat bot.</p>
                 <p><strong>For Discord Server Owners</strong>: Elevate your Discord server with our accompanying bot, offering streamlined moderation and engaging community features.</p>
             </section>
             <section>
-                <h2 class="title">Easy to Use</h2>
+                <h2>Easy to Use</h2>
                 <p><strong>User-Friendly Dashboard</strong>: Everything you need to manage your Twitch channel and Discord server is just a few clicks away.</p>
                 <p><strong>Real-time Insights</strong>: Stay up-to-date with real-time data about your Twitch stream, chat engagement, Discord server activity, and member interactions.</p>
             </section>
             <section>
-                <h2 class="title">Join Our Community</h2>
+                <h2>Join Our Community</h2>
                 <p>Become a part of the BotOfTheSpecter family and take your Twitch channel and Discord server to new heights. Whether you're a broadcaster or server owner, our bots are here to enhance your streaming and community experiences.</p>
                 <p>Connect with fellow streamers and server owners on our <a href="https://discord.com/invite/ANwEkpauHJ" target="_blank">Discord server</a>. Share tips, get support, and be part of the conversation.</p>
                 <p>Interested in learning more or getting involved? Check out our features, or if you're a developer, dive into the technical details on our <a href="https://github.com/YourStreamingTools/BotOfTheSpecter" target="_blank">GitHub page</a>.</p>
@@ -52,10 +47,8 @@
             </section>
         </div>
     </div>
-    <footer class="footer">
-        <div class="content has-text-centered">
-            &copy; 2023-<?php echo date("Y"); ?> BotOfTheSpecter - All Rights Reserved. | Server 1
-        </div>
+    <footer>
+        &copy; 2023-<?php echo date("Y"); ?> BotOfTheSpecter - All Rights Reserved. | Server 1
     </footer>
 </body>
 </html>
