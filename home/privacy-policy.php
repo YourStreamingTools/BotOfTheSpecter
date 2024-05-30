ef3ec73a992e8dd4dfbbe43fd827ba18b6478efc
gfaUnDead <contact@gfaundead.stream>
2024-05-30 18:28:45 +1000

    Update privacy-policy.php

--- home/privacy-policy.php
+++ home/privacy-policy.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BotOfTheSpecter - Privacy Policy</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-    <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
+  	<link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
+  	<link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@Tools4Streaming" />
     <meta name="twitter:title" content="BotOfTheSpecter" />
@@ -15,17 +14,13 @@
     <meta name="twitter:image" content="https://cdn.botofthespecter.com/BotOfTheSpecter.jpeg" />
 </head>
 <body>
-    <header class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">BotOfTheSpecter</h1>
-                <a href="https://botofthespecter.com/" class="button is-light is-outlined">HOME</a>
-            </div>
-        </div>
+    <header>
+        <h1>BotOfTheSpecter</h1>
+        <a href="https://botofthespecter.com/" class="login-button">HOME</a>
     </header>
-    <div class="container mt-6">
-        <div class="content">
-            <h1 class="title">Privacy Policy</h1>
+    <div class="container">
+        <div class="bio">
+            <h1>Privacy Policy</h1>
             <ol>
                 <li><strong>Information We Collect</strong>: We collect personal information you provide to us.</li>
                 <li><strong>How We Use Your Information</strong>: We use the information we collect to provide and improve our services.</li>
@@ -37,10 +32,8 @@
             </ol>
         </div>
     </div>
-    <footer class="footer">
-        <div class="content has-text-centered">
-            &copy; 2023-<?php echo date("Y"); ?> BotOfTheSpecter - All Rights Reserved. | Server 1
-        </div>
+    <footer>
+        &copy; 2023-<?php echo date("Y"); ?> BotOfTheSpecter - All Rights Reserved. | Server 1
     </footer>
 </body>
 </html>
