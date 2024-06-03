852ce95b808bac461bba4cf1e8ea3e13267b4ab1
gfaUnDead <contact@gfaundead.stream>
2024-06-03 17:10:55 +1000

    Update profile.php

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -74,98 +74,98 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!-- /Navigation -->
 
 <div class="container">
-<br>
-<h1 class="title is-4"><?php echo "$greeting! <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
-<table class="table is-fullwidth is-striped is-hoverable">
-  <tr>
-    <td>Your Username:</td>
-    <td><?php echo $username; ?></td>
-  </tr>
-  <tr>
-    <td>Display Name:</td>
-    <td><?php echo $twitchDisplayName; ?></td>
-  </tr>
-  <tr>
-    <td>You Joined:</td>
-    <td><span id="localSignupDate"></span></td>
-  </tr>
-  <tr>
-    <td>Your Last Login:</td>
-    <td><span id="localLastLogin"></span></td>
-  </tr>
-  <tr>
-    <td>Time Zone:</td>
-    <td><?php echo $timezone; ?></td>
-  </tr>
-  <tr>
-    <td>Weather Location:</td>
-    <td><?php echo $weather; ?></td>
-  </tr>
-</table>
-<p>Your API Key: <span class="api-key-wrapper api-text-black" style="display: none;"><?php echo $api_key; ?></span></p>
-<button type="button" class="button is-primary" id="show-api-key">Show API Key</button>
-<button type="button" class="button is-primary" id="hide-api-key" style="display:none;">Hide API Key</button>
-<br><br>
-<button type="button" class="button is-primary" onclick="showOBSInfo()">HOW TO USE THE OVERLAY</button>
-<br><br><br>
-<h2 class="title is-4">Update Profile</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <div class="field">
-    <label class="label" for="timezone">Timezone:</label>
-    <div class="control">
-      <input class="input" type="text" id="timezone" name="timezone" value="<?php echo $timezone; ?>">
+  <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
+  <br>
+  <table class="table is-fullwidth is-striped is-hoverable">
+    <tr>
+      <td>Your Username:</td>
+      <td><?php echo $username; ?></td>
+    </tr>
+    <tr>
+      <td>Display Name:</td>
+      <td><?php echo $twitchDisplayName; ?></td>
+    </tr>
+    <tr>
+      <td>You Joined:</td>
+      <td><span id="localSignupDate"></span></td>
+    </tr>
+    <tr>
+      <td>Your Last Login:</td>
+      <td><span id="localLastLogin"></span></td>
+    </tr>
+    <tr>
+      <td>Time Zone:</td>
+      <td><?php echo $timezone; ?></td>
+    </tr>
+    <tr>
+      <td>Weather Location:</td>
+      <td><?php echo $weather; ?></td>
+    </tr>
+  </table>
+  <p>Your API Key: <span class="api-key-wrapper api-text-black" style="display: none;"><?php echo $api_key; ?></span></p>
+  <button type="button" class="button is-primary" id="show-api-key">Show API Key</button>
+  <button type="button" class="button is-primary" id="hide-api-key" style="display:none;">Hide API Key</button>
+  <br><br>
+  <button type="button" class="button is-primary" onclick="showOBSInfo()">HOW TO USE THE OVERLAY</button>
+  <br><br><br>
+  <h2 class="title is-4">Update Profile</h2>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <div class="field">
+      <label class="label" for="timezone">Timezone:</label>
+      <div class="control">
+        <input class="input" type="text" id="timezone" name="timezone" value="<?php echo $timezone; ?>">
+      </div>
     </div>
-  </div>
-  <div class="field">
-    <label class="label" for="weather_location">Weather Location:</label>
-    <div class="control">
-      <input class="input" type="text" id="weather_location" name="weather_location" value="<?php echo $weather; ?>">
+    <div class="field">
+      <label class="label" for="weather_location">Weather Location:</label>
+      <div class="control">
+        <input class="input" type="text" id="weather_location" name="weather_location" value="<?php echo $weather; ?>">
+      </div>
     </div>
+    <div class="control">
+      <input type="submit" class="button is-primary" value="Submit">
+    </div>
+  </form>
+  <br>
+  <?php if (!empty($status)): ?>
+    <div class="notification is-primary">
+      <?php echo htmlspecialchars($status); ?>
+    </div>
+  <?php endif; ?>
   </div>
-  <div class="control">
-    <input type="submit" class="button is-primary" value="Submit">
-  </div>
-</form>
-<br>
-<?php if (!empty($status)): ?>
-  <div class="notification is-primary">
-    <?php echo htmlspecialchars($status); ?>
-  </div>
-<?php endif; ?>
-</div>
-
-<!-- Include the JavaScript files -->
-<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="/js/profile.js"></script>
-<script src="/js/obsbutton.js" defer></script>
-<script src="/js/timezone.js"></script>
-
-<!-- JavaScript code to convert and display the dates -->
-<script>
-  // Function to convert UTC date to local date in the desired format
-  function convertUTCToLocalFormatted(utcDateStr) {
-    const options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      hour12: true,
-      timeZoneName: 'short'
-    };
-    const utcDate = new Date(utcDateStr + ' UTC');
-    const localDate = new Date(utcDate.toLocaleString('en-US', { timeZone: '<?php echo $timezone; ?>' }));
-    const dateTimeFormatter = new Intl.DateTimeFormat('en-US', options);
-    return dateTimeFormatter.format(localDate);
-  }
-
-  // PHP variables holding the UTC date and time
-  const signupDateUTC = "<?php echo $signup_date_utc; ?>";
-  const lastLoginUTC = "<?php echo $last_login_utc; ?>";
-
-  // Display the dates in the user's local time zone
-  document.getElementById('localSignupDate').innerText = convertUTCToLocalFormatted(signupDateUTC);
-  document.getElementById('localLastLogin').innerText = convertUTCToLocalFormatted(lastLoginUTC);
-</script>
+  
+  <!-- Include the JavaScript files -->
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="/js/profile.js"></script>
+  <script src="/js/obsbutton.js" defer></script>
+  <script src="/js/timezone.js"></script>
+  
+  <!-- JavaScript code to convert and display the dates -->
+  <script>
+    // Function to convert UTC date to local date in the desired format
+    function convertUTCToLocalFormatted(utcDateStr) {
+      const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+        timeZoneName: 'short'
+      };
+      const utcDate = new Date(utcDateStr + ' UTC');
+      const localDate = new Date(utcDate.toLocaleString('en-US', { timeZone: '<?php echo $timezone; ?>' }));
+      const dateTimeFormatter = new Intl.DateTimeFormat('en-US', options);
+      return dateTimeFormatter.format(localDate);
+    }
+  
+    // PHP variables holding the UTC date and time
+    const signupDateUTC = "<?php echo $signup_date_utc; ?>";
+    const lastLoginUTC = "<?php echo $last_login_utc; ?>";
+  
+    // Display the dates in the user's local time zone
+    document.getElementById('localSignupDate').innerText = convertUTCToLocalFormatted(signupDateUTC);
+    document.getElementById('localLastLogin').innerText = convertUTCToLocalFormatted(lastLoginUTC);
+  </script>
 </body>
 </html>
