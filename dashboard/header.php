994472b04806d1d6724cad8057dda4287f76bb76
gfaUnDead <contact@gfaundead.stream>
2024-06-21 19:16:06 +1000

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -4,8 +4,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="css/bulma-custom.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-    <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
+    <link rel="icon" href="https://botofthespecter.yourcdnonline.com/logo.png">
+    <link rel="apple-touch-icon" href="https://botofthespecter.yourcdnonline.com/logo.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/11.0.0/markdown-it.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="/js/about.js"></script>
