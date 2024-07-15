ee2c57c6e665f695daff73b938931f0766f508cf
gfaUnDead <contact@gfaundead.stream>
2024-07-15 20:08:15 +1000

    Update stripe.js

--- dashboard/payments/stripe.js
+++ dashboard/payments/stripe.js
@@ -1,24 +1,4 @@
 var stripe = Stripe(''); // CHANGE TO MAKE THIS WORK
-var checkoutButton = document.getElementById('checkout-button');
-checkoutButton.addEventListener('click', function () {
-    fetch('/payments/payment.php', {
-        method: 'POST',
-    })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (session) {
-        return stripe.redirectToCheckout({ sessionId: session.id });
-    })
-    .then(function (result) {
-        if (result.error) {
-            alert(result.error.message);
-        }
-    })
-    .catch(function (error) {
-        console.error('Error:', error);
-    });
-});
 
 // Close notification
 document.addEventListener('DOMContentLoaded', () => {
