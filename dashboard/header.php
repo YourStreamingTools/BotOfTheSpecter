cf3c8b2d59acef307e9a3643ab96afb436335264
gfaUnDead <contact@gfaundead.stream>
2024-07-15 19:19:58 +1000

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -2,13 +2,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>BotOfTheSpecter - <?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-    <link rel="stylesheet" href="css/bulma-custom.css">
+    <link rel="stylesheet" href="../css/bulma-custom.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="icon" href="https://botofthespecter.yourcdnonline.com/logo.png">
     <link rel="apple-touch-icon" href="https://botofthespecter.yourcdnonline.com/logo.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/11.0.0/markdown-it.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="/js/about.js"></script>
-    <script src="/js/navbar.js" defer></script>
+    <script src="../js/about.js"></script>
+    <script src="../js/navbar.js" defer></script>
     <!-- <?php echo "User: $username | $twitchUserId | $authToken"; ?> -->
     <!-- <?php echo "python $botScriptPath -channel $username -channelid $twitchUserId -token $authToken -refresh $refreshToken >> $logPath"; ?> -->
