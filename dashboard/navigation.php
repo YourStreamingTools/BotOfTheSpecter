a8ecb95bd47a293ba0cc7a8681575df5d0f4428a
gfaUnDead <contact@gfaundead.stream>
2024-07-15 19:19:50 +1000

    Update navigation.php

--- dashboard/navigation.php
+++ dashboard/navigation.php
@@ -11,52 +11,52 @@
   </div>
   <div id="navbarBasic" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item" href="bot.php">Dashboard</a>
+      <a class="navbar-item" href="../bot.php">Dashboard</a>
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">Twitch Data</a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="mods.php">Your Mods</a>
-          <a class="navbar-item" href="followers.php">Your Followers</a>
-          <a class="navbar-item" href="subscribers.php">Your Subscribers</a>
-          <a class="navbar-item" href="vips.php">Your VIPs</a>
-          <a class="navbar-item" href="channel_rewards.php">Channel Point Rewards</a>
+          <a class="navbar-item" href="../mods.php">Your Mods</a>
+          <a class="navbar-item" href="../followers.php">Your Followers</a>
+          <a class="navbar-item" href="../subscribers.php">Your Subscribers</a>
+          <a class="navbar-item" href="../vips.php">Your VIPs</a>
+          <a class="navbar-item" href="../channel_rewards.php">Channel Point Rewards</a>
         </div>
       </div>
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">Logs</a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="logs.php">View Logs</a>
-          <a class="navbar-item" href="chat_history.php">Chat History</a>
+          <a class="navbar-item" href="../logs.php">View Logs</a>
+          <a class="navbar-item" href="../chat_history.php">Chat History</a>
         </div>
       </div>
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">Bot Messages</a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="known_users.php">Welcome Messages</a>
-          <a class="navbar-item" href="timed_messages.php">Timed Messages</a>
+          <a class="navbar-item" href="../known_users.php">Welcome Messages</a>
+          <a class="navbar-item" href="../timed_messages.php">Timed Messages</a>
         </div>
       </div>
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">Bot Counting</a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="counters.php">Counters</a>
-          <a class="navbar-item" href="edit_typos.php">Edit Typos</a>
-          <a class="navbar-item" href="edit_custom_counts.php">Edit Custom Counters</a>
+          <a class="navbar-item" href="../counters.php">Counters</a>
+          <a class="navbar-item" href="../edit_typos.php">Edit Typos</a>
+          <a class="navbar-item" href="../edit_custom_counts.php">Edit Custom Counters</a>
         </div>
       </div>
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">Bot Commands</a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="builtin.php">View Built-in Commands</a>
-          <a class="navbar-item" href="commands.php">View Custom Commands</a>
-          <a class="navbar-item" href="add-commands.php">Add Custom Command</a>
-          <a class="navbar-item" href="remove-commands.php">Remove Custom Command</a>
-          <a class="navbar-item" href="edit-commands.php">Edit Custom Command</a>
+          <a class="navbar-item" href="../builtin.php">View Built-in Commands</a>
+          <a class="navbar-item" href="../commands.php">View Custom Commands</a>
+          <a class="navbar-item" href="../add-commands.php">Add Custom Command</a>
+          <a class="navbar-item" href="../remove-commands.php">Remove Custom Command</a>
+          <a class="navbar-item" href="../edit-commands.php">Edit Custom Command</a>
         </div>
       </div>
-      <a class="navbar-item" href="discordbot.php">Discord Bot</a>
-      <a class="navbar-item" href="profile.php">Profile</a>
-      <a class="navbar-item" href="logout.php">Logout</a>
+      <a class="navbar-item" href="../discordbot.php">Discord Bot</a>
+      <a class="navbar-item" href="../profile.php">Profile</a>
+      <a class="navbar-item" href="../logout.php">Logout</a>
     </div>
     <div class="navbar-end">
       <div class="navbar-item">
