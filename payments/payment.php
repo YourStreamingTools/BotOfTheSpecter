bb6b6098c78df7e6a574f9b94b2ffc814271581b
gfaUnDead <contact@gfaundead.stream>
2024-07-15 19:09:35 +1000

    Create payment.php

---
+++ payments/payment.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+// Ensure the user is logged in
+if (!isset($_SESSION['access_token'])) {
+    header('Location: https://dashboard.botofthespecter.com/login.php');
+    exit();
+}
+
+$stripeSecretKey = ''; // CHANGE TO MAKE THIS WORK
+
+// Include Stripe PHP bindings
+require_once('stripe-php/init.php');
+
+\Stripe\Stripe::setApiKey($stripeSecretKey);
+
+header('Content-Type: application/json');
+
+try {
+    $DOMAIN = 'https://payments.botofthespecter.com';
+    $checkout_session = \Stripe\Checkout\Session::create([
+        'payment_method_types' => ['card'],
+        'line_items' => [[
+            'price_data' => [
+                'currency' => 'aud',
+                'product_data' => [
+                    'name' => 'Twitch Bot Paid Feature',
+                ],
+                'unit_amount' => 800,
+            ],
+            'quantity' => 1,
+        ]],
+        'customer_email' => $_SESSION['user_email'],
+        'mode' => 'payment',
+        'success_url' => $DOMAIN . '/success.php',
+        'cancel_url' => $DOMAIN . '/cancel.php',
+    ]);
+
+    echo json_encode(['id' => $checkout_session->id]);
+
+} catch (Error $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
+?>
