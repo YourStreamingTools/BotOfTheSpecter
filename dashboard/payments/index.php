43a0e0d1cdf02ee9d1809e587809d1b773fe1e78
GFAUnDead <contact@gfaundead.stream>
2024-07-22 12:38:29 +1000

    Update index.php

--- dashboard/payments/index.php
+++ dashboard/payments/index.php
@@ -93,6 +93,15 @@ $status = isset($_GET['status']) ? $_GET['status'] : null;
     <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$profileImageUrl' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
     <br>
     <h1 class="title">Premium Features</h1>
+    <h2 class="subtitle">
+        At YourStreamingTools, we believe businesses have a critically important role in combating climate change. 
+        As the team behind BotOfTheSpecter, we're dedicated to making a positive impact on the environment. 
+        We’re proud to fund next-generation carbon removal efforts. To support this cause, we're contributing 1% of our revenue to carbon removal.
+        <br>
+        <img src="https://cdn.botofthespecter.com/StripeClimate/StripeClimate-Small.png" width="128" height="128" alt="Stripe Climate">
+        <br>
+        <a href="https://climate.stripe.com/tPEkBr" target="_blank">Learn more</a>
+    </h2>
 
     <div class="card-container">
         <div class="card">
