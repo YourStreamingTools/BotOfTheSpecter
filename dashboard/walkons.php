193289e970239931affb405ac69fcb25d5fc83d3
gfaUnDead <contact@gfaundead.stream>
2024-07-23 21:03:33 +1000

    Create walkons.php

---
+++ dashboard/walkons.php
@@ -0,0 +1,142 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Initialize the session
+session_start();
+
+// check if user is logged in
+if (!isset($_SESSION['access_token'])) {
+    header('Location: login.php');
+    exit();
+}
+
+// Page Title
+$title = "Walkons";
+
+// Connect to database
+require_once "db_connect.php";
+
+// Fetch the user's data from the database based on the access_token
+$access_token = $_SESSION['access_token'];
+$userSTMT = $conn->prepare("SELECT * FROM users WHERE access_token = ?");
+$userSTMT->bind_param("s", $access_token);
+$userSTMT->execute();
+$userResult = $userSTMT->get_result();
+$user = $userResult->fetch_assoc();
+$user_id = $user['id'];
+$username = $user['username'];
+$twitchDisplayName = $user['twitch_display_name'];
+$twitch_profile_image_url = $user['profile_image'];
+$is_admin = ($user['is_admin'] == 1);
+$betaAccess = ($user['beta_access'] == 1);
+$twitchUserId = $user['twitch_user_id'];
+$broadcasterID = $twitchUserId;
+$authToken = $access_token;
+$refreshToken = $user['refresh_token'];
+$api_key = $user['api_key'];
+$timezone = 'Australia/Sydney';
+date_default_timezone_set($timezone);
+$greeting = 'Hello';
+include 'bot_control.php';
+include 'sqlite.php';
+
+$walkon_path = "/var/www/walkons/" . $username;
+$status = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!is_dir($walkon_path)) {
+        if (!mkdir($walkon_path, 0755, true)) {
+            exit;
+        }
+    }
+
+    $targetFile = $walkon_path . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOK = 1;
+    $fileType = strtolower(pathinfo($walkon_path, PATHINFO_EXTENSION));
+
+    if ($fileType != "mp3") {
+        $uploadOK = 0;
+        $status = "Failed to upload, only mp3 files are allowed.";
+    }
+
+    if ($uploadOK == 0) {
+        exit;
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $walkon_path)) {
+            $status = "File Uploaded.";
+        } else {
+            $status = "For some unknown reason, the file failed to upload.";
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Header -->
+        <?php include('header.php'); ?>
+        <!-- /Header -->
+    </head>
+<body>
+<!-- Navigation -->
+<?php include('navigation.php'); ?>
+<!-- /Navigation -->
+
+<div class="container">
+    <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
+    <br>
+    <h1 class="title is-4">Upload Walkons</h1>
+    <div class="upload-container">
+        <div class="drag-area" id="drag-area">
+            Drag & Drop files here or
+            <label for="filesToUpload">Browse Files</label>
+            <input type="file" name="filesToUpload[]" id="filesToUpload" multiple>
+        </div>
+        <form action="" method="POST" enctype="multipart/form-data">
+        <input type="submit" value="Upload MP3" name="submit">
+        </form>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        let dropArea = $('#drag-area');
+        let fileInput = $('#filesToUpload')
+
+        dropArea.on('dragover', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            dropArea.addClass('dragging');
+        });
+
+        dropArea.on('dragleave', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            dropArea.removeClass('dragging');
+        });
+
+        dropArea.on('drop', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            dropArea.removeClass('dragging');
+
+            let files = e.originalEvent.dataTransfer.files;
+            files.prop('file', files);
+
+            $('#uploadForm').submit();
+        });
+
+        dropArea.on('click', function() {
+            fileInput.click();
+        });
+
+        dropArea.on('change', function() {
+            $('#uploadForm').submit();
+        });
+    });
+</script>
+</body>
+</html>
