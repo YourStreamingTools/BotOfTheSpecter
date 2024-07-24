bf12181d247cd717a8400092d8076c225923e3a6
gfaUnDead <contact@gfaundead.stream>
2024-07-24 20:54:14 +1000

    Update overlays.php

--- dashboard/overlays.php
+++ dashboard/overlays.php
@@ -57,35 +57,54 @@ include 'sqlite.php';
     <h4 class="title is-4">OBS Studio, Streamlabs OBS, XSplit Broadcaster, Wirecast, vMix, Lightstream, and many more</h4>
     <h4 class="title is-4">To integrate with your streaming setup, simply add one or more of the following links to a browser source in your streaming software, appending your API key from the profile page:</h4>
     <br>
-    <div class="box">
-        <h4 class="title is-4">All Overlays:</h4>
-        <p><em>This URL includes any and all overlays we offer, automatically added and updated.
-            <br>You only need to add this link once, and any new overlays will be included automatically:</em></p>
-        <code>https://overlay.botofthespecter.com/?code=API_KEY_HERE</code>
-    </div>
-    <div class="box">
-        <h4 class="title is-4">Death Overlay:</h4>
-        <p><em>For best results, set Width to 450 and Height to 350:</em></p>
-        <code>https://overlay.botofthespecter.com/deaths.php?code=API_KEY_HERE</code>
-    </div>
-    <div class="box">
-        <h4 class="title is-4">Stream End Credits:</h4>
-        <p><em>(Coming Soon)</em></p>
-        <code>https://overlay.botofthespecter.com/credits.php?code=API_KEY_HERE</code>
-    </div>
-    <div class="box">
-        <h4 class="title is-4">All Audio:</h4>
-        <p><em>This URL includes any and all audio alerts we offer, automatically added and updated.
-            <br>You only need to add this link once, and any new audio alerts will be included automatically:</em></p>
-        <code>https://overlay.botofthespecter.com/alert.php?code=API_KEY_HERE</code>
-    </div>
-    <div class="box">
-        <h4 class="title is-4">TTS Only:</h4>
-        <code>https://overlay.botofthespecter.com/tts.php?code=API_KEY_HERE</code>
-    </div>
-    <div class="box">
-        <h4 class="title is-4">Walkons Only:</h4>
-        <code>https://overlay.botofthespecter.com/walkons.php?code=API_KEY_HERE</code>
+    <div class="columns is-desktop is-multiline">
+        <div class="column is-one-third">
+            <div class="box">
+                <h4 class="title is-4">All Overlays:</h4>
+                <p><em>This URL includes any and all overlays we offer, automatically added and updated.
+                    <br>You only need to add this link once, and any new overlays will be included automatically:</em></p>
+                <code>https://overlay.botofthespecter.com/?code=API_KEY_HERE</code>
+            </div>
+        </div>
+        <div class="column is-one-third">
+            <div class="box">
+                <h4 class="title is-4">Death Overlay:</h4>
+                <p><em>Display only the death overlay when the death commands are triggered in chat:
+                    <br>"!deaths", "!deathadd", and "!deathremove".
+                    <br>For best results, set Width to 450 and Height to 350:</em></p>
+                <code>https://overlay.botofthespecter.com/deaths.php?code=API_KEY_HERE</code>
+            </div>
+        </div>
+        <div class="column is-one-third">
+            <div class="box">
+                <h4 class="title is-4">Stream End Credits:</h4>
+                <p><em>As it sounds, displays a scrolling list of all the people that have attended and supported the stream.
+                    <br>(Coming Soon)</em></p>
+                <code>https://overlay.botofthespecter.com/credits.php?code=API_KEY_HERE</code>
+            </div>
+        </div>
+        <div class="column is-one-third">
+            <div class="box">
+                <h4 class="title is-4">All Audio:</h4>
+                <p><em>This URL includes any and all audio alerts we offer, automatically added and updated.
+                    <br>You only need to add this link once, and any new audio alerts will be included automatically:</em></p>
+                <code>https://overlay.botofthespecter.com/alert.php?code=API_KEY_HERE</code>
+            </div>
+        </div>
+        <div class="column is-one-third">
+            <div class="box">
+                <h4 class="title is-4">TTS Only:</h4>
+                <p><em>Only hear the Text To Speech audio.</em></p>
+                <code>https://overlay.botofthespecter.com/tts.php?code=API_KEY_HERE</code>
+            </div>
+        </div>
+        <div class="column is-one-third">
+            <div class="box">
+                <h4 class="title is-4">Walkons Only:</h4>
+                <p><em>Only hear the Walkon audio that you've set for each user.</em></p>
+                <code>https://overlay.botofthespecter.com/walkons.php?code=API_KEY_HERE</code>
+            </div>
+        </div>
     </div>
 </div>
 
