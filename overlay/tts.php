6b0fc1cd6a5fd009f53a6038d865df1b8866041a
gfaUnDead <contact@gfaundead.stream>
2024-07-24 20:16:52 +1000

    Create tts.php

---
+++ overlay/tts.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WebSocket TTS Audio Notifications</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="https://cdn.socket.io/4.0.0/socket.io.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const socket = io('wss://websocket.botofthespecter.com:8080');
+            const urlParams = new URLSearchParams(window.location.search);
+            const code = urlParams.get('code');
+            if (!code) {
+                alert('No code provided in the URL');
+                return;
+            }
+
+            socket.on('connect', () => {
+                console.log('Connected to WebSocket server');
+                socket.emit('REGISTER', { code: code });
+            });
+
+            socket.on('disconnect', () => {
+                console.log('Disconnected from WebSocket server');
+            });
+
+            socket.on('WELCOME', (data) => {
+                console.log('Server says:', data.message);
+            });
+
+            socket.on('NOTIFY', (data) => {
+                console.log('Notification:', data);
+                alert(data.message);
+            });
+
+            // Listen for TTS audio events
+            socket.on('TTS', (data) => {
+                console.log('TTS Audio file path:', data.audio_file);
+                const audio = new Audio(data.audio_file);
+                audio.volume = 0.8;
+                audio.autoplay = true;
+                audio.addEventListener('canplaythrough', () => {
+                    console.log('Audio can play through without buffering');
+                });
+                audio.addEventListener('error', (e) => {
+                    console.error('Error occurred while loading the audio file:', e);
+                    alert('Failed to load audio file');
+                });
+
+                setTimeout(() => {
+                    audio.play().catch(error => {
+                        console.error('Error playing audio:', error);
+                        alert('Click to play audio');
+                    });
+                }, 100); // 100ms delay
+            });
+        });
+    </script>
+</head>
+<body>
+</body>
+</html>
