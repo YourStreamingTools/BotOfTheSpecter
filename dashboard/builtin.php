78d532d0c4671089c674952f5c150ca900a74d4f
gfaUnDead <contact@gfaundead.stream>
2024-07-24 21:04:32 +1000

    Update builtin.php

--- dashboard/builtin.php
+++ dashboard/builtin.php
@@ -144,5 +144,6 @@ function toggleStatus(commandName, isChecked) {
 }
 </script>
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="/js/search.js"></script>
 </body>
 </html>
