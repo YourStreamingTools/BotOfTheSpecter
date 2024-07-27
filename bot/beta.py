96ac4054cf6357447cebc269207dd7775bde23a7
gfaUnDead <contact@gfaundead.stream>
2024-07-27 14:13:20 +1000

    Update beta.py

--- bot/beta.py
+++ bot/beta.py
@@ -1319,7 +1319,7 @@ class BotOfTheSpecter(commands.Bot):
             await sqldb.ensure_closed()
 
     @commands.command(name='weather')
-    async def weather_command(self, ctx, location: str = None) -> None:
+    async def weather_command(self, ctx, *, location: str = None) -> None:
         sqldb = await get_mysql_connection()
         try:
             async with sqldb.cursor() as cursor:
