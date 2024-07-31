bec3a337ff627a40de9eced2e712c91186c52b26
GFAUnDead <contact@gfaundead.stream>
2024-07-31 10:47:47 +1000

    Update bot_control.php

--- dashboard/bot_control.php
+++ dashboard/bot_control.php
@@ -17,13 +17,21 @@ $BetaBotScriptPath = "/var/www/bot/beta.py";
 $BetaStatusScriptPath = "/var/www/bot/beta_status.py";
 $BetaLogPath = "/var/www/logs/script/{$username}_beta.txt";
 
-$statusOutput = getBotStatus($statusScriptPath, $username, $logPath);
-$botSystemStatus = checkBotRunning($statusScriptPath, $username, $logPath);
-$betaStatusOutput = getBotStatus($BetaStatusScriptPath, $username, $BetaLogPath);
-$betaBotSystemStatus = checkBotRunning($BetaStatusScriptPath, $username, $BetaLogPath);
+// Define variables for Discord bot
+$discordBotScriptPath = "/var/www/bot/discordbot.py";
+$discordStatusScriptPath = "/var/www/bot/discordstatus.py";
+$discordLogPath = "/var/www/logs/script/{$username}_discord.txt";
+
+$statusOutput = getBotsStatus($statusScriptPath, $username, $logPath);
+$botSystemStatus = checkBotsRunning($statusScriptPath, $username, $logPath);
+$betaStatusOutput = getBotsStatus($BetaStatusScriptPath, $username, $BetaLogPath);
+$betaBotSystemStatus = checkBotsRunning($BetaStatusScriptPath, $username, $BetaLogPath);
+$discordStatusOutput = getBotsStatus($discordStatusScriptPath, $username, $discordLogPath);
+$discordBotSystemStatus = checkBotsRunning($discordStatusScriptPath, $username, $discordLogPath);
 
 $directory = dirname($logPath);
 $betaDirectory = dirname($BetaLogPath);
+$discordDirectory = dirname($discordLogPath);
 
 // Check if directories exist, if not, create them
 if (!file_exists($directory)) {
@@ -35,6 +43,12 @@ if (!file_exists($directory)) {
 if (!file_exists($betaDirectory)) {
     if (!mkdir($betaDirectory, 0777, true)) {
         echo "Failed to create directory: $betaDirectory";
+        exit;
+    }
+}
+if (!file_exists($discordDirectory)) {
+    if (!mkdir($discordDirectory, 0777, true)) {
+        echo "Failed to create directory: $discordDirectory";
         exit;
     }
 }
@@ -52,40 +66,61 @@ if (($file = fopen($BetaLogPath, 'w')) === false) {
 }
 fclose($file);
 
+if (($file = fopen($discordLogPath, 'w')) === false) {
+    echo "Failed to create/open the file: $discordLogPath";
+    exit;
+}
+fclose($file);
+
 // Initialize status message variables
-$statusOutput = getBotStatus($statusScriptPath, $username, $logPath);
-$botSystemStatus = checkBotRunning($statusScriptPath, $username, $logPath);
-$betaStatusOutput = getBotStatus($BetaStatusScriptPath, $username, $BetaLogPath);
-$betaBotSystemStatus = checkBotRunning($BetaStatusScriptPath, $username, $BetaLogPath);
+$statusOutput = getBotsStatus($statusScriptPath, $username, $logPath);
+$botSystemStatus = checkBotsRunning($statusScriptPath, $username, $logPath);
+$betaStatusOutput = getBotsStatus($BetaStatusScriptPath, $username, $BetaLogPath);
+$betaBotSystemStatus = checkBotsRunning($BetaStatusScriptPath, $username, $BetaLogPath);
+$discordStatusOutput = getBotsStatus($discordStatusScriptPath, $username, $discordLogPath);
+$discordBotSystemStatus = checkBotsRunning($discordStatusScriptPath, $username, $discordLogPath);
 
 // Handle standard bot actions
 if (isset($_POST['runBot'])) {
-    $statusOutput = handleBotAction('run', $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath);
+    $statusOutput = handleTwitchBotAction('run', $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath);
 }
 
 if (isset($_POST['killBot'])) {
-    $statusOutput = handleBotAction('kill', $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath);
+    $statusOutput = handleTwitchBotAction('kill', $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath);
 }
 
 if (isset($_POST['restartBot'])) {
-    $statusOutput = handleBotAction('restart', $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath);
+    $statusOutput = handleTwitchBotAction('restart', $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath);
 }
 
 // Handle beta bot actions
 if (isset($_POST['runBetaBot'])) {
-    $betaStatusOutput = handleBotAction('run', $BetaBotScriptPath, $BetaStatusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $BetaLogPath);
+    $betaStatusOutput = handleTwitchBotAction('run', $BetaBotScriptPath, $BetaStatusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $BetaLogPath);
 }
 
 if (isset($_POST['killBetaBot'])) {
-    $betaStatusOutput = handleBotAction('kill', $BetaBotScriptPath, $BetaStatusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $BetaLogPath);
+    $betaStatusOutput = handleTwitchBotAction('kill', $BetaBotScriptPath, $BetaStatusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $BetaLogPath);
 }
 
 if (isset($_POST['restartBetaBot'])) {
-    $betaStatusOutput = handleBotAction('restart', $BetaBotScriptPath, $BetaStatusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $BetaLogPath);
+    $betaStatusOutput = handleTwitchBotAction('restart', $BetaBotScriptPath, $BetaStatusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $BetaLogPath);
+}
+
+// Handle Discord bot actions
+if (isset($_POST['runDiscordBot'])) {
+    $discordStatusOutput = handleDiscordBotAction('run', $discordBotScriptPath, $discordStatusScriptPath, $username, $discordLogPath);
+}
+
+if (isset($_POST['killDiscordBot'])) {
+    $discordStatusOutput = handleDiscordBotAction('kill', $discordBotScriptPath, $discordStatusScriptPath, $username, $discordLogPath);
+}
+
+if (isset($_POST['restartDiscordBot'])) {
+    $discordStatusOutput = handleDiscordBotAction('restart', $discordBotScriptPath, $discordStatusScriptPath, $username, $discordLogPath);
 }
 
 // Function to handle bot actions
-function handleBotAction($action, $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath) {
+function handleTwitchBotAction($action, $botScriptPath, $statusScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath) {
     $statusOutput = shell_exec("python $statusScriptPath -channel $username");
     $pid = intval(preg_replace('/\D/', '', $statusOutput));
     $message = '';
@@ -133,6 +168,55 @@ function handleBotAction($action, $botScriptPath, $statusScriptPath, $username, 
     return $message;
 }
 
+// Function to handle Discord Bot Actions
+function handleDiscordBotAction($action, $discordBotScriptPath, $discordStatusScriptPath, $username, $discordLogPath) {
+    $statusOutput = shell_exec("python $discordStatusScriptPath -channel $username");
+    $pid = intval(preg_replace('/\D/', '', $statusOutput));
+    $message = '';
+
+    switch ($action) {
+        case 'run':
+            if ($pid > 0) {
+                $message = "<div class='status-message'>Discord bot is already running. PID $pid.</div>";
+            } else {
+                startDiscordBot($discordBotScriptPath, $username, $discordLogPath);
+                $statusOutput = shell_exec("python $discordStatusScriptPath -channel $username");
+                $pid = intval(preg_replace('/\D/', '', $statusOutput));
+                if ($pid > 0) {
+                    $message = "<div class='status-message'>Discord bot started successfully. PID $pid.</div>";
+                } else {
+                    $message = "<div class='status-message error'>Failed to start the Discord bot. Please check the configuration or server status.</div>";
+                }
+            }
+            break;
+        case 'kill':
+            if ($pid > 0) {
+                killBot($pid);
+                $message = "<div class='status-message'>Discord bot stopped successfully.</div>";
+            } else {
+                $message = "<div class='status-message error'>Discord bot is not running.</div>";
+            }
+            break;
+        case 'restart':
+            if ($pid > 0) {
+                killBot($pid);
+                startDiscordBot($discordBotScriptPath, $username, $discordLogPath);
+                $statusOutput = shell_exec("python $discordStatusScriptPath -channel $username");
+                $pid = intval(preg_replace('/\D/', '', $statusOutput));
+                if ($pid > 0) {
+                    $message = "<div class='status-message'>Discord bot restarted. PID $pid.</div>";
+                } else {
+                    $message = "<div class='status-message error'>Failed to restart the Discord bot.</div>";
+                }
+            } else {
+                $message = "<div class='status-message error'>Discord bot is not running.</div>";
+            }
+            break;
+    }
+
+    return $message;
+}
+
 function isBotRunning($statusScriptPath, $username, $logPath) {
     $pid = getBotPID($statusScriptPath, $username, $logPath);
     return ($pid > 0);
@@ -143,7 +227,7 @@ function getBotPID($statusScriptPath, $username, $logPath) {
     return intval(preg_replace('/\D/', '', $statusOutput));
 }
 
-function getBotStatus($statusScriptPath, $username, $logPath) {
+function getBotsStatus($statusScriptPath, $username, $logPath) {
     $statusOutput = shell_exec("python $statusScriptPath -channel $username");
     $pid = intval(preg_replace('/\D/', '', $statusOutput));
     if ($pid > 0) {
@@ -153,7 +237,7 @@ function getBotStatus($statusScriptPath, $username, $logPath) {
     }
 }
 
-function checkBotRunning($statusScriptPath, $username, $logPath) {
+function checkBotsRunning($statusScriptPath, $username, $logPath) {
     $statusOutput = shell_exec("python $statusScriptPath -channel $username");
     $pid = intval(preg_replace('/\D/', '', $statusOutput));
     return ($pid > 0);
@@ -161,6 +245,13 @@ function checkBotRunning($statusScriptPath, $username, $logPath) {
 
 function startBot($botScriptPath, $username, $twitchUserId, $authToken, $refreshToken, $api_key, $logPath) {
     $command = "python $botScriptPath -channel $username -channelid $twitchUserId -token $authToken -refresh $refreshToken -apitoken $api_key >> $logPath 2>&1 &";
+    $output = shell_exec($command);
+    sleep(1);
+    return !(empty($output) || strpos($output, 'error') !== false);
+}
+
+function startDiscordBot($discordBotScriptPath, $username, $discordLogPath) {
+    $command = "python $discordBotScriptPath -channel $username >> $discordLogPath 2>&1 &";
     $output = shell_exec($command);
     sleep(1);
     return !(empty($output) || strpos($output, 'error') !== false);
@@ -175,6 +266,7 @@ function killBot($pid) {
 // Display running versions if bots are running
 $versionRunning = '';
 $betaVersionRunning = '';
+$discordVersionRunning = '';
 
 if ($botSystemStatus) {
     $versionRunning = getRunningVersion($versionFilePath, $newVersion);
@@ -182,6 +274,12 @@ if ($botSystemStatus) {
 
 if ($betaBotSystemStatus) {
     $betaVersionRunning = getRunningVersion($betaVersionFilePath, $betaNewVersion, 'beta');
+}
+
+if ($discordBotSystemStatus) {
+    $discordVersionRunning = "<div class='status-message'>Discord bot is running.</div>";
+} else {
+    $discordVersionRunning = "<div class='status-message error'>Discord bot is NOT RUNNING.</div>";
 }
 
 function getRunningVersion($versionFilePath, $newVersion, $type = '') {
