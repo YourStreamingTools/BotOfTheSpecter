0f8bda7e9e468d9f5762bb94f473bf69fad60c61
gfaUnDead <contact@gfaundead.stream>
2024-08-01 22:34:55 +1000

    Update beta.py

--- bot/beta.py
+++ bot/beta.py
@@ -886,7 +886,7 @@ class BotOfTheSpecter(twitch_commands.Bot):
                                 if '(user)' in response:
                                     user_mention = re.search(r'@(\w+)', messageContent)
                                     if user_mention:
-                                        user_name = user_mention
+                                        user_name = user_mention.group(1)
                                     else:
                                         # Default to message author's name
                                         user_name = messageAuthor
