c3f031cb8690991ec83fde50b4a4ffa6eb42d00f
GFAUnDead <contact@gfaundead.stream>
2024-08-06 13:12:02 +1000

    Update database.php

--- dashboard/todolist/database.php
+++ dashboard/todolist/database.php
@@ -1,9 +1,4 @@
 <?php
 $db = new PDO("mysql:host=sql.botofthespecter.com;dbname={$username}", "USERNAME", "PASSWORD");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// Fetch all custom commands
-$getCommands = $db->query("SELECT * FROM custom_commands");
-$commands = $getCommands->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
