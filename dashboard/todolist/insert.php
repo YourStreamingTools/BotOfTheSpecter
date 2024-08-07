0518c306338a39551d1b61e988f804ca1ec19285
GFAUnDead <contact@gfaundead.stream>
2024-08-07 10:16:57 +1000

    Update insert.php

--- dashboard/todolist/insert.php
+++ dashboard/todolist/insert.php
@@ -68,54 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   </head>
 <body>
 <!-- Navigation -->
-<nav class="navbar is-spaced" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="dashboard.php">
-      YourListOnline
-    </a>
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item" href="dashboard.php">Dashboard</a>
-      <a class="navbar-item is-active" href="insert.php">Add</a>
-      <a class="navbar-item" href="remove.php">Remove</a>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">Update</a>
-        <div class="navbar-dropdown">
-          <a class="navbar-item" href="update_objective.php">Update Objective</a>
-          <a class="navbar-item" href="update_category.php">Update Objective Category</a>
-        </div>
-      </div>
-      <a class="navbar-item" href="completed.php">Completed</a>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">Categories</a>
-        <div class="navbar-dropdown">
-          <a class="navbar-item" href="categories.php">View Categories</a>
-          <a class="navbar-item" href="add_category.php">Add Category</a>
-        </div>
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">Profile</a>
-        <div class="navbar-dropdown">
-          <a class="navbar-item" href="obs_options.php">OBS Viewing Options</a>
-        </div>
-      </div>
-    </div>
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <button id="dark-mode-toggle" class="button is-dark"><i class="icon-toggle-dark-mode"></i></button>
-      </div>
-      <div class="navbar-item">
-        <a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a>
-      </div>
-    </div>
-  </div>
-</nav>
+<?php include('navigation.php'); ?>
 <!-- /Navigation -->
 
 <div class="container">
