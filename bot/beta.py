56b61e3eaa041f56417de6d33c9ce9792bed4262
gfaUnDead <contact@gfaundead.stream>
2024-08-14 21:55:31 +1000

    Update beta.py

--- bot/beta.py
+++ bot/beta.py
@@ -3137,7 +3137,7 @@ class BotOfTheSpecter(twitch_commands.Bot):
                     else:
                         await ctx.send("Invalid format. Please use the format: !convert <amount><unit> <to_unit> or !convert $<amount><from_currency> <to_currency>")
                 except Exception as e:
-                    await ctx.send("Failed to convert, please try again.")
+                    await ctx.send("Failed to convert, please try again. Please ensure the format is: !convert <amount><unit> <to_unit> or !convert $<amount><from_currency> <to_currency>")
                     sanitized_error = str(e).replace(EXCHANGE_RATE_API, '[API_KEY]')
                     api_logger.error(f"An error occurred: {sanitized_error}")
         finally:
