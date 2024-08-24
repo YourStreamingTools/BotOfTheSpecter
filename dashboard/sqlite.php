37d1bf768bd99ecb7b22a92dd4b8e19d5c9fa3a2
gfaUnDead <contact@gfaundead.stream>
2024-08-25 01:38:59 +1000

    Update sqlite.php

--- dashboard/sqlite.php
+++ dashboard/sqlite.php
@@ -33,10 +33,6 @@ try {
     $getTypos = $db->query("SELECT * FROM user_typos ORDER BY typo_count DESC");
     $typos = $getTypos->fetchAll(PDO::FETCH_ASSOC);
 
-    // Fetch lurkers
-    $getLurkers = $db->query("SELECT user_id, start_time FROM lurk_times");
-    $lurkers = $getLurkers->fetchAll(PDO::FETCH_ASSOC);
-
     // Fetch total deaths
     $getTotalDeaths = $db->query("SELECT death_count FROM total_deaths");
     $totalDeaths = $getTotalDeaths->fetch(PDO::FETCH_ASSOC);
@@ -81,62 +77,7 @@ try {
     $getProfileSettings = $db->query("SELECT * FROM profile");
     $profileData = $getProfileSettings->fetchAll(PDO::FETCH_ASSOC);
 
-    // Calculate lurk durations for each user
-    foreach ($lurkers as $key => $lurker) {
-        $startTime = new DateTime($lurker['start_time']);
-        $currentTime = new DateTime();
-        $interval = $currentTime->diff($startTime);
-
-        $timeStringParts = [];
-        if ($interval->y > 0) {
-            $timeStringParts[] = "{$interval->y} year(s)";
-        }
-        if ($interval->m > 0) {
-            $timeStringParts[] = "{$interval->m} month(s)";
-        }
-        if ($interval->d > 0) {
-            $timeStringParts[] = "{$interval->d} day(s)";
-        }
-        if ($interval->h > 0) {
-            $timeStringParts[] = "{$interval->h} hour(s)";
-        }
-        if ($interval->i > 0) {
-            $timeStringParts[] = "{$interval->i} minute(s)";
-        }
-        $lurkers[$key]['lurk_duration'] = implode(', ', $timeStringParts);
-    }
 } catch (PDOException $e) {
     echo 'Error: ' . $e->getMessage();
-}
-
-// Prepare the Twitch API request for user data
-$userIds = array_column($lurkers, 'user_id');
-$userIdParams = implode('&id=', $userIds);
-$twitchApiUrl = "https://api.twitch.tv/helix/users?id=" . $userIdParams;
-$clientID = 'mrjucsmsnri89ifucl66jj1n35jkj8';
-$headers = [
-    "Client-ID: $clientID",
-    "Authorization: Bearer $authToken",
-];
-
-// Execute the Twitch API request
-$ch = curl_init($twitchApiUrl);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
-
-// Decode the JSON response
-$userData = json_decode($response, true);
-
-// Check if data exists and is not null
-if (isset($userData['data']) && is_array($userData['data'])) {
-    // Map user IDs to usernames
-    $usernames = [];
-    foreach ($userData['data'] as $user) {
-        $usernames[$user['id']] = $user['display_name'];
-    }
-} else {
-    $usernames = [];
 }
 ?>
