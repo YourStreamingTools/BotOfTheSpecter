cfc2f983c84bbf81db43f9675f9bd6dfe6e1d81c
gfaUnDead <contact@gfaundead.stream>
2024-08-25 00:06:09 +1000

    Create bot_points.php

---
+++ dashboard/bot_points.php
@@ -0,0 +1,63 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Initialize the session
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['access_token'])) {
+    header('Location: login.php');
+    exit();
+}
+
+// Page Title
+$title = "Edit Bot Points Settings";
+
+// Connect to database
+require_once "db_connect.php";
+
+// Fetch the user's data from the database based on the access_token
+$access_token = $_SESSION['access_token'];
+$userSTMT = $conn->prepare("SELECT * FROM users WHERE access_token = ?");
+$userSTMT->bind_param("s", $access_token);
+$userSTMT->execute();
+$userResult = $userSTMT->get_result();
+$user = $userResult->fetch_assoc();
+$user_id = $user['id'];
+$username = $user['username'];
+$twitchDisplayName = $user['twitch_display_name'];
+$twitch_profile_image_url = $user['profile_image'];
+$is_admin = ($user['is_admin'] == 1);
+$twitchUserId = $user['twitch_user_id'];
+$broadcasterID = $twitchUserId;
+$authToken = $access_token;
+$refreshToken = $user['refresh_token'];
+$timezone = 'Australia/Sydney';
+date_default_timezone_set($timezone);
+$greeting = 'Hello';
+include 'bot_control.php';
+include 'sqlite.php';
+$status = '';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Header -->
+    <?php include('header.php'); ?>
+    <!-- /Header -->
+</head>
+<body>
+<!-- Navigation -->
+<?php include('navigation.php'); ?>
+<!-- /Navigation -->
+
+<div class="container">
+    <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
+    <br>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+</body>
+</html>
