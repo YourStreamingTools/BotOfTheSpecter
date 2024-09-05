445871d9f1d0e94b2bf3a64e8585c10d68ad29ad
GFAUnDead <contact@gfaundead.stream>
2024-09-05 15:15:05 +1000

    Update alert.php

--- overlay/alert.php
+++ overlay/alert.php
@@ -18,7 +18,7 @@
 
             socket.on('connect', () => {
                 console.log('Connected to WebSocket server');
-                socket.emit('REGISTER', { code: code });
+                socket.emit('REGISTER', { code: code, name: 'All Audio Overlay' });
             });
 
             socket.on('disconnect', () => {
