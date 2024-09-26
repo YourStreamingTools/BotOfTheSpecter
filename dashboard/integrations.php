c8a6c9310fae9813b4355b343d1910354ef4552a
gfaUnDead <contact@gfaundead.stream>
2024-09-26 17:13:29 +1000

    Update integrations.php

--- dashboard/integrations.php
+++ dashboard/integrations.php
@@ -53,42 +53,39 @@ include 'sqlite.php';
   <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
   <br>
   <!-- Fourthwall Integration -->
-  <div class="content">
-      <h2 class="subtitle">Fourthwall Integration</h2>
-      <p>Follow the steps below to integrate Specter with your Fourthwall account:</p>
-      <ol>
-          <li>Login to your Fourthwall admin dashboard.</li>
-          <li>On the left-hand menu, click Settings.</li>
-          <li>In the Site Settings page, find and click the For developers link.</li>
-          <li>Click Create webhook in the webhooks section.</li>
-          <li>
-              In the URL field, enter:
-              <code>https://api.botofthespecter.com/fourthwall?api_key=</code>
-              Make sure to append your API key, which can be found on the Profile page.
-          </li>
-          <li>
-              From the "Add Event" list, choose any or all of the following events:
-              <ul>
-                  <li>Order placed</li>
-                  <li>Gift purchase</li>
-                  <li>Donation</li>
-                  <li>Subscription purchased</li>
-              </ul>
-          </li>
-      </ol>
-      <p>That's it! Your Fourthwall account is now integrated with Specter.</p>
+  <div class="content content-card">
+    <h2 class="subtitle">Fourthwall Integration</h2>
+    <p>Follow the steps below to integrate Specter with your Fourthwall account:</p>
+    <ol>
+      <li>Login to your Fourthwall admin dashboard.</li>
+      <li>On the left-hand menu, click Settings.</li>
+      <li>In the Site Settings page, find and click the For developers link.</li>
+      <li>Click Create webhook in the webhooks section.</li>
+      <li>In the URL field, enter: <br>
+        <code>https://api.botofthespecter.com/fourthwall?api_key=</code> <br>
+        Make sure to append your API key, which can be found on the Profile page.
+      </li>
+      <li>From the "Add Event" list, choose any or all of the following events:
+        <ul>
+          <li>Order placed</li>
+          <li>Gift purchase</li>
+          <li>Donation</li>
+          <li>Subscription purchased</li>
+        </ul>
+      </li>
+    </ol>
+    <p>That's it! Your Fourthwall account is now integrated with Specter.</p>
   </div>
   <!-- Ko-Fi Integration -->
-  <div class="content">
+  <div class="content content-card">
     <h2 class="subtitle">Ko-Fi Integration</h2>
     <p>Follow the steps below to integrate Specter with your Ko-Fi account:</p>
     <ol>
       <li>Log into your Ko-Fi account.</li>
       <li>When the manage page loads, on the left-hand side, under Stream Alerts, click the three dots where it says More.</li>
       <li>In the "More" section, click the API option.</li>
-      <li>
-        In the webhook URL field, enter:
-        <code>https://api.botofthespecter.com/kofi?api_key=</code>
+      <li>In the webhook URL field, enter: <br>
+        <code>https://api.botofthespecter.com/kofi?api_key=</code> <br>
         Make sure to append your API key, which can be found on the Profile page of the Specter dashboard.
       </li>
       <li>Once you've entered the URL, click the Update button.</li>
