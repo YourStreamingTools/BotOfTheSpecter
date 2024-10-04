efc8519e0b759854e6e019e20ec713c1d5c878b3
gfaUnDead <contact@gfaundead.stream>
2024-10-05 01:30:09 +1000

    Update stripe_customer.php

--- dashboard/payments/stripe_customer.php
+++ dashboard/payments/stripe_customer.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['access_token'])) {
+    header('Location: ../login.php');
+    exit();
+}
+
 require_once('stripe-php/init.php');
 $stripeSecretKey = ''; // CHANGE TO MAKE THIS WORK
 \Stripe\Stripe::setApiKey($stripeSecretKey);
