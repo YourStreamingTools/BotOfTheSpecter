c454b0d3cec2f6e33aa43b17d684a18f10803886
gfaUnDead <contact@gfaundead.stream>
2024-10-05 04:13:23 +1000

    Update navigation.php

--- dashboard/navigation.php
+++ dashboard/navigation.php
@@ -45,7 +45,7 @@
       <a class="navbar-item" href="../profile.php">Profile</a>
       <a class="navbar-item" href="../overlays.php">Overlays</a>
       <a class="navbar-item" href="../integrations.php">Integrations</a>
-      <a class="navbar-item" href="../payments/index.php">Payments</a>
+      <a class="navbar-item" href="../payments.php">Payments</a>
       <a class="navbar-item" href="../todolist/index.php">To Do List</a>
       <a class="navbar-item" href="../logout.php">Logout</a>
     </div>
