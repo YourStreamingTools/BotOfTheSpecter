9f605746e6c7c8ae917c6b08d83bb7c56fe22ec3
GFAUnDead <contact@gfaundead.stream>
2024-10-18 09:02:32 +1100

    Update discordbot.php

--- dashboard/discordbot.php
+++ dashboard/discordbot.php
@@ -1,12 +1,8 @@
 <?php 
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
-
 // Initialize the session
 session_start();
 
-// check if user is logged in
+// Check if the user is logged in
 if (!isset($_SESSION['access_token'])) {
     header('Location: login.php');
     exit();
