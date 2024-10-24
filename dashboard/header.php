68c6282be4a34ffb72a562ed57fb34df0c05edd1
GFAUnDead <contact@gfaundead.stream>
2024-10-24 15:06:30 +1100

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -1,4 +1,4 @@
-<meta charset="utf-8" />
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>BotOfTheSpecter - <?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
