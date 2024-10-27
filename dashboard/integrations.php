8ba3f7652663ccda54dc6c1ad033242ff80584bd
GFAUnDead <contact@gfaundead.stream>
2024-10-28 08:58:39 +1100

    Update integrations.php

--- dashboard/integrations.php
+++ dashboard/integrations.php
@@ -95,7 +95,5 @@ include 'sqlite.php';
 </div>
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
-<script>$(document).foundation();</script>
 </body>
 </html>
