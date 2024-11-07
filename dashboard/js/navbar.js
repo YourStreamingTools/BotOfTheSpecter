5c17f30b32511dce542d699d1bffe31fdb0b75bb
GFAUnDead <contact@gfaundead.stream>
2024-11-08 09:10:46 +1100

    Revert "Update navbar.js"
    This reverts commit c34cb3dcc6cfe0474588c11438f109a7f558edec.

--- dashboard/js/navbar.js
+++ dashboard/js/navbar.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
         // Add a click event on each of them
@@ -16,8 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-
-function toggleNavbar() {
-  var navbar = document.getElementById("navbarBasic");
-  navbar.classList.toggle("is-active");
-}
