2a3e0478f2412c11c9ded23ebf1fbc155f43c040
GFAUnDead <contact@gfaundead.stream>
2024-11-08 10:27:54 +1100

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <link rel="stylesheet" href="../css/bulma-custom.css?v=4.9">
+    <link rel="stylesheet" href="../css/bulma-custom.css?v=5.0">
     <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
     <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/11.0.0/markdown-it.min.js"></script>
