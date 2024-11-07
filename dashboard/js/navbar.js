c34cb3dcc6cfe0474588c11438f109a7f558edec
GFAUnDead <contact@gfaundead.stream>
2024-11-08 09:09:21 +1100

    Update navbar.js

--- dashboard/js/navbar.js
+++ dashboard/js/navbar.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
         // Add a click event on each of them
@@ -17,3 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+function toggleNavbar() {
+  var navbar = document.getElementById("navbarBasic");
+  navbar.classList.toggle("is-active");
+}
