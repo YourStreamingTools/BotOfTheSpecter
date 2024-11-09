6909d215dc4260ba2097e4e348c385b3c9545a0d
gfaUnDead <contact@gfaundead.stream>
2024-11-09 19:40:07 +1100

    Update protection.php

--- dashboard/protection.php
+++ dashboard/protection.php
@@ -34,7 +34,56 @@ date_default_timezone_set($timezone);
 $greeting = 'Hello';
 include 'bot_control.php';
 include 'sqlite.php';
+$message = '';
 
+// Fetch protection settings
+$getProtection = $db->query("SELECT * FROM protection LIMIT 1");
+$settings = $getProtection->fetchAll(PDO::FETCH_ASSOC);
+$currentSettings = isset($settings[0]['url_blocking']) ? $settings[0]['url_blocking'] : 'False';
+
+// Update database with settings
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // URL Blocking Settings
+    if (isset($_POST['url_blocking'])) {
+        $url_blocking = $_POST['url_blocking'] == 'True' ? 'True' : 'False';
+        $stmt = $db->prepare("UPDATE protection SET url_blocking = ?");
+        $stmt->bindParam(1, $url_blocking, PDO::PARAM_STR);
+        if ($stmt->execute()) {
+            $message .= "URL Blocking setting updated successfully.<br>";
+        } else {
+            $message .= "Failed to update your URL Blocking settings.<br>";
+            error_log("Error updating URL blocking: " . implode(", ", $stmt->errorInfo()));
+        }
+    } else {
+        $message .= "Please select either True or False.<br>";
+    }
+
+    // Whitelist Links
+    if (isset($_POST['whitelist_link'])) {
+        $whitelist_link = $_POST['whitelist_link'];
+        $stmt = $db->prepare("INSERT INTO link_whitelist (link) VALUES (?)");
+        $stmt->bindParam(1, $whitelist_link, PDO::PARAM_STR);
+        if ($stmt->execute()) {
+            $message .= "Link added to the whitelist.<br>";
+        } else {
+            $message .= "Failed to add the link to the whitelist.<br>";
+            error_log("Error inserting whitelist link: " . implode(", ", $stmt->errorInfo()));
+        }
+    }
+
+    // Blacklist Links
+    if (isset($_POST['blacklist_link'])) {
+        $blacklist_link = $_POST['blacklist_link'];
+        $stmt = $db->prepare("INSERT INTO link_blacklisting (link) VALUES (?)");
+        $stmt->bindParam(1, $blacklist_link, PDO::PARAM_STR);
+        if ($stmt->execute()) {
+            $message .= "Link added to the blacklist.<br>";
+        } else {
+            $message .= "Failed to add the link to the blacklist.<br>";
+            error_log("Error inserting blacklist link: " . implode(", ", $stmt->errorInfo()));
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -52,6 +101,54 @@ include 'sqlite.php';
     <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
     <br>
     <h1 class="title">Chat Protection</h1>
+    <?php if (!empty($message)): ?>
+        <div class="notification is-primary has-text-black has-text-weight-bold">
+            <?php echo $message; ?>
+        </div>
+    <?php endif; ?>
+    <!-- Enable or Disable URL Blocking -->
+    <form action="" method="post">
+        <div class="field">
+            <label for="url_blocking">Enable URL Blocking:</label>
+            <div class="control">
+                <div class="select">
+                    <select name="url_blocking" id="url_blocking">
+                        <option value="True"<?php echo $currentSettings == 'True' ? ' selected' :'';?>>True</option>
+                        <option value="False"<?php echo $currentSettings == 'False' ? ' selected' :'';?>>False</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="field">
+            <input type="submit" name="submit" value="Update"></input>
+        </div>
+    </form>
+    <br>
+    <!-- Add a new link to the White List -->
+    <form action="" method="post">
+        <div class="field">
+            <label for="whitelist_link">Enter Link to Whitelist:</label>
+            <div class="control">
+                <input class="input" type="url" name="whitelist_link" id="whitelist_link" placeholder="Enter a URL" required>
+            </div>
+        </div>
+        <div class="field">
+            <input type="submit" name="submit" value="Add to Whitelist"></input>
+        </div>
+    </form>
+    <br>
+    <!-- Add a new link to the Black List -->
+    <form action="" method="post">
+        <div class="field">
+            <label for="blacklist_link">Enter Link to Blacklist:</label>
+            <div class="control">
+                <input class="input" type="url" name="blacklist_link" id="blacklist_link" placeholder="Enter a URL" required>
+            </div>
+        </div>
+        <div class="field">
+            <input type="submit" name="submit" value="Add to Blacklist"></input>
+        </div>
+    </form>
 </div>
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
