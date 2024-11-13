679fb888d42f0f1d9473d0953b17b417141867c9
GFAUnDead <contact@gfaundead.stream>
2024-11-14 08:39:16 +1100

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -4,12 +4,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <link rel="stylesheet" href="../css/bulma-custom.css?v=5.0">
-    <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
-    <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/11.0.0/markdown-it.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="../js/about.js"></script>
-    <script src="../js/navbar.js" defer></script>
+    <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
+    <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
+    <link rel="stylesheet" href="../css/bulma-custom.css?v=5.0">
+    <script src="../js/about.js?v=1.0"></script>
+    <script src="../js/navbar.js?v=1.4"></script>
     <!-- <?php echo "User: $username | $twitchUserId | $authToken"; ?> -->
     <!-- <?php echo "python $botScriptPath -channel $username -channelid $twitchUserId -token $authToken -refresh $refreshToken -apitoken $api_key >> $logPath"; ?> -->
