886f8e958b4ea11d6c1c35e07b31aa40b62df246
GFAUnDead <contact@gfaundead.stream>
2024-11-14 12:29:15 +1100

    Update usr_database.php

--- dashboard/usr_database.php
+++ dashboard/usr_database.php
@@ -299,7 +299,7 @@ try {
         'joke_settings ' => "
             CREATE TABLE IF NOT EXISTS joke_settings (
                 id INT(11) NOT NULL AUTO_INCREMENT,
-                blacklist TEXT
+                blacklist TEXT,
                 PRIMARY KEY (id)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci",
     ];
