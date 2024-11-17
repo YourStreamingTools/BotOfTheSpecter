400728ee392e6cc488ff5cc5712277c03cc8c911
gfaUnDead <contact@gfaundead.stream>
2024-11-17 23:34:36 +1100

    Update commands.php

--- dashboard/commands.php
+++ dashboard/commands.php
@@ -21,7 +21,6 @@ foreach ($profileData as $profile) {
   $weather = $profile['weather_location'];
 }
 date_default_timezone_set($timezone);
-$greeting = 'Hello';
 
 // Check if the update request is sent via POST
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['command']) && isset($_POST['status'])) {
@@ -49,7 +48,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['command']) && isset($_
 <!-- /Navigation -->
 
 <div class="container">
-  <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
   <br>
   <div class="columns">
     <div class="column">
