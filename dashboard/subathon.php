95564b945c3e84d11f51f400c382fe5659d49511
gfaUnDead <contact@gfaundead.stream>
2024-11-17 23:42:01 +1100

    Update subathon.php

--- dashboard/subathon.php
+++ dashboard/subathon.php
@@ -21,7 +21,6 @@ foreach ($profileData as $profile) {
   $weather = $profile['weather_location'];
 }
 date_default_timezone_set($timezone);
-$greeting = 'Hello';
 $message = '';
 
 // Fetch the current subathon settings
@@ -63,7 +62,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_settings'])) {
 <!-- /Navigation -->
 
 <div class="container">
-    <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
     <br>
     <h1 class="title">Subathon Settings</h1>
     <?php if ($message): ?>
