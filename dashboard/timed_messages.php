40835042dac3ea44c6e0e41e9735eec194186b6a
gfaUnDead <contact@gfaundead.stream>
2024-11-17 23:41:58 +1100

    Update timed_messages.php

--- dashboard/timed_messages.php
+++ dashboard/timed_messages.php
@@ -21,7 +21,6 @@ foreach ($profileData as $profile) {
   $weather = $profile['weather_location'];
 }
 date_default_timezone_set($timezone);
-$greeting = 'Hello';
 
 // Initialize variables for messages or errors
 $successMessage = "";
@@ -133,7 +132,6 @@ if ($displayMessageData) {
 <!-- /Navigation -->
 
 <div class="container">
-    <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
     <br>
     <div class="notification is-danger">
         Important Notice: This feature is currently under heavy development and testing. As such, it may not work as expected at this time.<br>
