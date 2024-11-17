d3714e40f39d8964bf86adea2ceb973492fa9300
gfaUnDead <contact@gfaundead.stream>
2024-11-17 23:38:28 +1100

    Update followers.php

--- dashboard/followers.php
+++ dashboard/followers.php
@@ -21,7 +21,6 @@ foreach ($profileData as $profile) {
   $weather = $profile['weather_location'];
 }
 date_default_timezone_set($timezone);
-$greeting = 'Hello';
 
 // Handle AJAX request to load followers
 if (isset($_GET['load']) && $_GET['load'] == 'followers') {
@@ -128,7 +127,6 @@ function fetchFollowers($url, $authToken, $clientID) {
 <!-- /Navigation -->
 
 <div class="container">
-  <h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
   <br>
   <!-- Followers Content Container -->
   <div id="followers-content">
