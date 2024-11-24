df56fa8d65afb3214cacbd9224b6e0c5187a24b4
gfaUnDead <contact@gfaundead.stream>
2024-11-24 18:49:43 +1100

    Update update_category.php

--- dashboard/todolist/update_category.php
+++ dashboard/todolist/update_category.php
@@ -83,8 +83,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <div class="container">
 <br>
-<h1 class="title"><?php echo "$greeting, $twitchDisplayName <img id='profile-image' class='round-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>"; ?></h1>
-<br>
 <?php if ($num_rows < 1) { echo '<h3 class="has-text-danger">There are no rows to edit</h3>'; } else { echo "<h2 class='subtitle'>Please pick which row to update on your list:</h2>"; ?>
 <form method="POST">
 <button type="submit" name="submit" class="button is-primary">Update All</button>
