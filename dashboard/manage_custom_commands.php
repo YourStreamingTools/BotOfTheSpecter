0cdef32672b363f7a4e2325ef6a638bc7e317f87
gfaUnDead <contact@gfaundead.stream>
2024-11-24 19:54:54 +1100

    Update manage_custom_commands.php

--- dashboard/manage_custom_commands.php
+++ dashboard/manage_custom_commands.php
@@ -174,77 +174,76 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </div>
 
 <div class="modal" id="customVariablesModal">
-        <div class="modal-background"></div>
-        <div class="modal-card custom-width">
-            <header class="modal-card-head has-background-dark">
-                <p class="modal-card-title has-text-white">Custom Variables to use while adding commands</p>
-                <button class="delete" aria-label="close" id="closeModalButton"></button>
-            </header>
-            <section class="modal-card-body has-background-dark has-text-white">
-                <div class="columns is-desktop is-multiline">
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(count)</span><br>
-                        This variable counts how many times the specific command has been used and shows that number. The count tracks how many times this command has been used, not others.<br>
-                        <span class="has-text-weight-bold">Example:</span><br>
-                        <code>This command has been used (count) times.</code><br>
-                        <span class="has-text-weight-bold">In Twitch Chat:</span><br>
-                        <code>"This command has been used 5 times."</code><br><br>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(usercount)</span><br>
-                        This variable counts how many times a specific user has used the command. The count is tracked for each user individually, so each user will have their own usage count stored in the database.<br>
-                        <span class="has-text-weight-bold">Example:</span><br>
-                        <code>This user has used this command (usercount) times.</code><br>
-                        <span class="has-text-weight-bold">In Twitch Chat:</span><br>
-                        <code>"This user has used this command 3 times."</code><br><br>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(customapi.URL)</span><br>This gets information from a URL and posts it in chat.<br>You can use this to get jokes, weather, or any other data from a website.
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>(customapi.https://api.botofthespecter.com/joke?api_key=APIKEY)</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"Why don’t skeletons fight each other? They don’t have the guts."</code>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(daysuntil.DATE)</span><br>This shows how many days until a specific date, like a holiday or event.
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>There are (daysuntil.2024-12-25) days until Christmas.</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"There are 75 days until Christmas."</code>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(user)</span> | <span class="has-text-weight-bold variable-title">(author)</span><br>This lets you tag someone by name when they use the command.<br>If no one is tagged, it will tag the person who used the command.<br>To always tag the user who issued the command use (author).
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>(author) is saying that (user) is awesome!</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"BotOfTheSpecter is saying that John is awesome!"</code>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(random.pick.*)</span><br>This randomly picks an item from a list you provide. It could be used to pick random items, people, or anything else.
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>Your spirit animal is: (random.pick.cat.dog.eagle.tiger)</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"Your spirit animal is: tiger"</code>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(math.*)</span><br>This solves simple math problems.
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>2+2 = (math.2+2)</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"2+2 = 4"</code>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(command.COMMAND)</span><br>This allows you to trigger other commands inside of one command.<br>You can combine multiple commands to post different messages.
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>Use these raid calls: (command.raid1) (command.raid2) (command.raid3)</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span>
-                        <br> "Use these raid calls:"
-                        <br> "Raid 1 message."
-                        <br> "Raid 2 message."
-                        <br> "Raid 3 message."
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(random.number)</span><br>This picks a random number between two numbers you specify, or by default between 0 and 100.<br>You can specify a range by using a dot followed by the numbers, like (random.number.1-1000), which will give you a random number within that range.
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>You've broken (random.number.1-1000) hearts!</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>You've broken 583 hearts!"</code>
-                    </div>
-                    <div class="column is-4">
-                        <span class="has-text-weight-bold variable-title">(random.percent)</span><br>This generates a random percentage between 0% and 100%, or any custom range you define.<br>You can specify a range by using a dot followed by the numbers, like (random.percent.0-200), which will give you a random percentage between those two numbers.
-                        <br><span class="has-text-weight-bold">Example:</span><br><code>You have a (random.percent.0-200) chance of winning this game.</code>
-                        <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"You have a 167% chance of winning this game."</code>
-                    </div>
+    <div class="modal-background"></div>
+    <div class="modal-card custom-width">
+        <header class="modal-card-head has-background-dark">
+            <p class="modal-card-title has-text-white">Custom Variables to use while adding commands</p>
+            <button class="delete" aria-label="close" id="closeModalButton"></button>
+        </header>
+        <section class="modal-card-body has-background-dark has-text-white">
+            <div class="columns is-desktop is-multiline">
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(count)</span><br>
+                    This variable counts how many times the specific command has been used and shows that number. The count tracks how many times this command has been used, not others.<br>
+                    <span class="has-text-weight-bold">Example:</span><br>
+                    <code>This command has been used (count) times.</code><br>
+                    <span class="has-text-weight-bold">In Twitch Chat:</span><br>
+                    <code>"This command has been used 5 times."</code><br><br>
                 </div>
-            </section>
-        </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(usercount)</span><br>
+                    This variable counts how many times a specific user has used the command. The count is tracked for each user individually, so each user will have their own usage count stored in the database.<br>
+                    <span class="has-text-weight-bold">Example:</span><br>
+                    <code>This user has used this command (usercount) times.</code><br>
+                    <span class="has-text-weight-bold">In Twitch Chat:</span><br>
+                    <code>"This user has used this command 3 times."</code><br><br>
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(customapi.URL)</span><br>This gets information from a URL and posts it in chat.<br>You can use this to get jokes, weather, or any other data from a website.
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>(customapi.https://api.botofthespecter.com/joke?api_key=APIKEY)</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"Why don’t skeletons fight each other? They don’t have the guts."</code>
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(daysuntil.DATE)</span><br>This shows how many days until a specific date, like a holiday or event.
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>There are (daysuntil.2024-12-25) days until Christmas.</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"There are 75 days until Christmas."</code>
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(user)</span> | <span class="has-text-weight-bold variable-title">(author)</span><br>This lets you tag someone by name when they use the command.<br>If no one is tagged, it will tag the person who used the command.<br>To always tag the user who issued the command use (author).
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>(author) is saying that (user) is awesome!</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"BotOfTheSpecter is saying that John is awesome!"</code>
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(random.pick.*)</span><br>This randomly picks an item from a list you provide. It could be used to pick random items, people, or anything else.
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>Your spirit animal is: (random.pick.cat.dog.eagle.tiger)</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"Your spirit animal is: tiger"</code>
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(math.*)</span><br>This solves simple math problems.
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>2+2 = (math.2+2)</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"2+2 = 4"</code>
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(command.COMMAND)</span><br>This allows you to trigger other commands inside of one command.<br>You can combine multiple commands to post different messages.
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>Use these raid calls: (command.raid1) (command.raid2) (command.raid3)</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span>
+                    <br> "Use these raid calls:"
+                    <br> "Raid 1 message."
+                    <br> "Raid 2 message."
+                    <br> "Raid 3 message."
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(random.number)</span><br>This picks a random number between two numbers you specify, or by default between 0 and 100.<br>You can specify a range by using a dot followed by the numbers, like (random.number.1-1000), which will give you a random number within that range.
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>You've broken (random.number.1-1000) hearts!</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>You've broken 583 hearts!"</code>
+                </div>
+                <div class="column is-4">
+                    <span class="has-text-weight-bold variable-title">(random.percent)</span><br>This generates a random percentage between 0% and 100%, or any custom range you define.<br>You can specify a range by using a dot followed by the numbers, like (random.percent.0-200), which will give you a random percentage between those two numbers.
+                    <br><span class="has-text-weight-bold">Example:</span><br><code>You have a (random.percent.0-200) chance of winning this game.</code>
+                    <br><span class="has-text-weight-bold">In Twitch Chat:</span><br><code>"You have a 167% chance of winning this game."</code>
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 
