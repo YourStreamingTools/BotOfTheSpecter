9ff71c3150121fc5f4d29d1f6ad9e82d3511d8b5
GFAUnDead <contact@gfaundead.stream>
2024-12-06 09:54:11 +1100

    Update header.php

--- dashboard/moderator/header.php
+++ dashboard/moderator/header.php
@@ -8,8 +8,6 @@
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <link rel="icon" href="https://cdn.botofthespecter.com/logo.png">
     <link rel="apple-touch-icon" href="https://cdn.botofthespecter.com/logo.png">
-    <link rel="stylesheet" href="../css/bulma-custom.css?v=5.0">
-    <script src="../js/about.js?v=1.0"></script>
-    <script src="../js/navbar.js?v=1.4"></script>
-    <!-- <?php echo "User: $username | $twitchUserId | $authToken"; ?> -->
-    <!-- <?php echo "python $botScriptPath -channel $username -channelid $twitchUserId -token $authToken -refresh $refreshToken -apitoken $api_key >> $logPath"; ?> -->
+    <link rel="stylesheet" href="../../css/bulma-custom.css?v=5.0">
+    <script src="../../js/about.js?v=1.0"></script>
+    <script src="../../js/navbar.js?v=1.4"></script>
