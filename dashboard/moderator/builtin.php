86dd4d84fbfa7ea08b9a2b363e1c8e886fcc0f99
GFAUnDead <contact@gfaundead.stream>
2024-12-06 09:58:39 +1100

    Update builtin.php

--- dashboard/moderator/builtin.php
+++ dashboard/moderator/builtin.php
@@ -18,7 +18,7 @@ $title = "Built-in Bot Commands";
 require_once "db_connect.php";
 include 'userdata.php';
 include 'bot_control.php';
-include 'sqlite.php';
+include 'user_db.php';
 foreach ($profileData as $profile) {
   $timezone = $profile['timezone'];
   $weather = $profile['weather_location'];
