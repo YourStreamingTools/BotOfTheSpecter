09fda715b7d6ed1748832cc6375d27fcbef25a92
gfaUnDead <contact@gfaundead.stream>
2024-12-26 17:55:39 +1100

    Update database.php

--- specterbotapp/home/database.php
+++ specterbotapp/home/database.php
@@ -1,18 +1,23 @@
 <?php
 $host = $_SERVER['HTTP_HOST'];
 $host_parts = explode('.', $host);
-$username = isset($host_parts[0]) ? $host_parts[0] : 'website';
-$database = $username;
+
+// Check if the host is "specterbot.app" or "www.specterbot.app"
+if (strpos($host, 'specterbot.app') !== false) {
+    $username = 'website';
+} else {
+    $username = isset($host_parts[0]) ? $host_parts[0] : 'website';
+}
 
 $servername = "sql.botofthespecter.com";
 $db_username = "";
 $db_password = "";
 
-$conn = new mysqli($servername, $db_username, $db_password, $database);
+$conn = new mysqli($servername, $db_username, $db_password, $username);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected successfully to the database: " . $database;
+$connection = "Connected successfully to the database: " . $username . "";
 ?>
