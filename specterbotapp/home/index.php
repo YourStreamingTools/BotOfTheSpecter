d31b12f9eb87af20f9f84f1e468b5c1bca7c7170
gfaUnDead <contact@gfaundead.stream>
2025-01-02 16:41:53 -0500

    Update index.php

--- specterbotapp/home/index.php
+++ specterbotapp/home/index.php
@@ -124,7 +124,7 @@ $loginURL = $authUrl . '?client_id=' . $clientId . '&redirect_uri=' . urlencode(
     <header>
         <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                a class="navbar-item" href="#"><img src="logo.png" alt="BotOfTheSpecter Logo"></a>
+                <a class="navbar-item" href="../"><img src="logo.png" alt="BotOfTheSpecter Logo"></a>
                 <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
