10300f86a90d63ea1a2f6f4aa376984b919b4cc3
gfaUnDead <contact@gfaundead.stream>
2025-01-15 20:26:28 -0500

    Update walkons.php

--- dashboard/walkons.php
+++ dashboard/walkons.php
@@ -78,8 +78,8 @@ $current_storage_used = calculateStorageUsed([$walkon_path, $soundalert_path]);
 $storage_percentage = ($current_storage_used / $max_storage_size) * 100;
 $remaining_storage = $max_storage_size - $current_storage_used;
 $max_upload_size = $remaining_storage;
-ini_set('upload_max_filesize', $max_upload_size);
-ini_set('post_max_size', $max_upload_size);
+// ini_set('upload_max_filesize', $max_upload_size);
+// ini_set('post_max_size', $max_upload_size);
 
 // Handle file upload
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES["filesToUpload"])) {
