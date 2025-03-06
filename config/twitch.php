1c6c90a885e881052987d4c0a730c1ba7567af8c
gfaUnDead <contact@gfaundead.stream>
2025-03-07 01:21:11 +1100

    Update twitch.php

--- config/twitch.php
+++ config/twitch.php
@@ -2,10 +2,10 @@
 $use_backup = true;
 if ($use_backup) {
     $clientID = "";     // CHANGE TO MAKE THIS WORK
-    $ClientSecret = ""; // CHANGE TO MAKE THIS WORK
+    $clientSecret = ""; // CHANGE TO MAKE THIS WORK
 } else {
     $clientID = "";     // CHANGE TO MAKE THIS WORK
-    $ClientSecret = ""; // CHANGE TO MAKE THIS WORK
+    $clientSecret = ""; // CHANGE TO MAKE THIS WORK
 }
 $redirectURI = 'https://dashboard.botofthespecter.com/login.php';
 ?>
