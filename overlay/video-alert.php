734b75a49c2bc0c451c24e4877af6dd6940ee485
gfaUnDead <contact@gfaundead.stream>
2025-03-10 02:56:51 +1100

    Update video-alert.php

--- overlay/video-alert.php
+++ overlay/video-alert.php
@@ -83,7 +83,7 @@
                 socket.on('connect', () => {
                     console.log('Connected to WebSocket server');
                     reconnectAttempts = 0;
-                    socket.emit('REGISTER', { code: code, name: 'Video Overlay' });
+                    socket.emit('REGISTER', { code: code, channel:'Overlay', name: 'Video Alerts' });
                 });
 
                 socket.on('disconnect', () => {
