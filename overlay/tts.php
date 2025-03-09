e6c25e519a4ae799047e400069d5a479e774ede9
gfaUnDead <contact@gfaundead.stream>
2025-03-10 02:56:48 +1100

    Update tts.php

--- overlay/tts.php
+++ overlay/tts.php
@@ -68,7 +68,7 @@
                 socket.on('connect', () => {
                     console.log('Connected to WebSocket server');
                     reconnectAttempts = 0;
-                    socket.emit('REGISTER', { code: code, name: 'TTS Overlay' });
+                    socket.emit('REGISTER', { code: code, channel:'Overlay', name: 'TTS' });
                 });
 
                 socket.on('disconnect', () => {
