bde42d6c0f6dccf2a7229808dc4e99bfe4f03251
gfaUnDead <contact@gfaundead.stream>
2025-03-10 02:56:54 +1100

    Update weather.php

--- overlay/weather.php
+++ overlay/weather.php
@@ -51,7 +51,7 @@ if ($username) {
                 socket.on('connect', () => {
                     console.log('Connected to WebSocket server');
                     reconnectAttempts = 0;
-                    socket.emit('REGISTER', { code: code, name: 'Weather Overlay' });
+                    socket.emit('REGISTER', { code: code, channel:'Overlay', name: 'Weather' });
                 });
 
                 socket.on('disconnect', () => {
