f7b840e00493d3532781242b4022e808566c8093
gfaUnDead <contact@gfaundead.stream>
2025-03-15 20:55:06 +1100

    Update channel_rewards.php

--- dashboard/channel_rewards.php
+++ dashboard/channel_rewards.php
@@ -100,7 +100,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['syncRewards'])) {
         <p>You can use the following custom variables in the message to send to chat when a reward is triggered.</p>
         <ul>
           <li><span class="has-text-weight-bold">(user)</span>: This will tag the user that has used the channel point reward.</li>
-          <li><span class="has-text-weight-bold" style="color: #000000;">(usercount)</span>: This will count how many times a user has used the channel point reward. </li>
+          <li><span class="has-text-weight-bold">(usercount)</span>: This will count how many times a user has used the channel point reward. </li>
         </ul>
       </td>
     </table>
