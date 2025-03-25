d73e926513b08f7153b19b4d0647fa17cbf3e545
gfaUnDead <contact@gfaundead.stream>
2025-03-26 08:24:46 +1100

    Create restricted.php

---
+++ dashboard/restricted.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BotOfTheSpecter - Twitch Login</title>
+    <link rel="icon" href="https://botofthespecter.yourcdnonline.com/logo.png" sizes="32x32" />
+    <link rel="icon" href="https://botofthespecter.yourcdnonline.com/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://botofthespecter.yourcdnonline.com/logo.png" />
+    <meta name="msapplication-TileImage" content="https://botofthespecter.yourcdnonline.com/logo.png" />
+</head>
+<body><?php echo "<p>$info</p>";?></body></html>
