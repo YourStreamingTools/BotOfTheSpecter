37b81545d265220a5361a743abe11abd8945ad91
gfaUnDead <contact@gfaundead.stream>
2025-03-26 00:26:43 +1100

    Update streaming.php

--- dashboard/streaming.php
+++ dashboard/streaming.php
@@ -1,9 +1,4 @@
 <?php 
-// Display PHP errors
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Initialize the session
 session_start();
 $today = new DateTime();
