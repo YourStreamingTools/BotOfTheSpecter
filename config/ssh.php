7cae30325030df4bc740d892a3210274c88bd2c4
gfaUnDead <contact@gfaundead.stream>
2025-03-25 23:36:18 +1100

    Update ssh.php

--- config/ssh.php
+++ config/ssh.php
@@ -1,5 +1,11 @@
 <?php
+// SSH Information
 $ssh_host = '';
 $ssh_username = '';
 $ssh_password = '';
+
+// Storage Server Information
+$storage_server_au_syd1_host = '';
+$storage_server_au_syd1_username = '';
+$storage_server_au_syd1_password = '';
 ?>
