a2a8abff0e4133bbea7d766b381606ef9ecdab8e
GFAUnDead <94876497+GFAUnDead@users.noreply.github.com>
2025-03-26 12:15:27 +1100

    Update ssh.php

--- config/ssh.php
+++ config/ssh.php
@@ -5,7 +5,7 @@ $ssh_username = '';
 $ssh_password = '';
 
 // Storage Server Information
-$storage_server_au_syd1_host = '';
-$storage_server_au_syd1_username = '';
-$storage_server_au_syd1_password = '';
+$storage_server_au_east_1_host = '';
+$storage_server_au_east_1_username = '';
+$storage_server_au_east_1_password = '';
 ?>
