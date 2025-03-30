52179bd9c75d7a35b28d144a429f09b7fbedea55
gfaUnDead <contact@gfaundead.stream>
2025-03-31 00:55:53 +1100

    Create cloudflare.php

---
+++ config/cloudflare.php
@@ -0,0 +1,13 @@
+<?php
+// Cloudflare API configuration
+$api_key = '';
+$email = '';
+$zone_id = '';
+
+// Cloudflare R2 bucket configuration
+$account_id = '';
+$access_key_id = '';
+$secret_access_key = '';
+$bucket_name = '';
+$r2_bucket_url = "https://{$account_id}.r2.cloudflarestorage.com";
+?>
