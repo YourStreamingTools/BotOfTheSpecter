4b367abcc1c886df8c5c3ba6d15138a566b9a42c
GFAUnDead <94876497+GFAUnDead@users.noreply.github.com>
2025-04-01 11:16:59 +1100

    Update privacy-policy.php

--- home/privacy-policy.php
+++ home/privacy-policy.php
@@ -49,15 +49,41 @@
 <!-- Main Content -->
 <section class="section">
     <div class="container">
-        <div class="has-text-light">
+        <div class="content has-text-light">
             <h1 class="title has-text-light">Privacy Policy</h1>
-            <p>Information We Collect: We collect personal information you provide to us.</p>
-            <p>How We Use Your Information: We use the information we collect to provide and improve our services.</p>
-            <p>Sharing Your Information: We may share your information with third-party service providers.</p>
-            <p>Data Retention: We retain your personal information for as long as necessary.</p>
-            <p>Your Rights: You have the right to access, correct, or delete your personal information.</p>
-            <p>Changes to Our Privacy Policy: We reserve the right to update or modify this Privacy Policy at any time.</p>
-            <p>Contact Us: If you have any questions, please contact us at [questions@botofthespecter.com].</p>
+            <p class="has-text-weight-bold">1. Introduction</p>
+            <p><span class="has-text-weight-bold">BotOfTheSpecter</span>, operating under the business name <span class="has-text-weight-bold">YourStreamingTools</span> and registered as a subsidiary of <span class="has-text-weight-bold">LochStudios, Australia Business Number 20 447 022 747</span> ("Company," "we," "us," or "our") is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, and protect your personal information when you visit our website ("Site") or use our services.</p>
+            <p class="has-text-weight-bold">2. Information We Collect</p>
+            <p><span class="has-text-weight-bold">2.1. Personal Information:</span> We collect personal information that you provide directly to us, including but not limited to your name, email address, phone number, billing address, and payment details, when you register an account, make a purchase, subscribe to our newsletter, submit a contact form, or otherwise interact with our Site. This information is used to provide you with our services, process transactions, and communicate with you.</p>
+            <p><span class="has-text-weight-bold">2.2. Usage Data:</span> We automatically collect information about your interactions with our Site, such as your IP address, browser type, operating system, referring URLs, pages visited, time spent on pages, and other diagnostic data. This data helps us analyze trends, administer the Site, track user movements, and gather demographic information for internal use.</p>
+            <p><span class="has-text-weight-bold">2.3. Payment Information:</span> We utilize third-party payment processors to handle payment transactions. We collect limited payment information, such as transaction IDs, but we do not store your full credit card details on our servers. The storage and processing of your payment information is governed by the privacy policies and security measures of our third-party payment processors.</p>
+            <p><span class="has-text-weight-bold">2.4. Cookies and Tracking Technologies:</span> We use cookies, web beacons, and other tracking technologies to enhance your experience on our Site, personalize content, and analyze traffic. These technologies may collect information about your browsing behavior and preferences. You can control the use of cookies through your browser settings, but disabling cookies may limit your access to certain features of the Site.</p>
+            <p class="has-text-weight-bold">3. How We Use Your Information</p>
+            <p><span class="has-text-weight-bold">3.1. Provide and Maintain Services:</span> We use your personal information to provide, maintain, and improve our services, process payments, fulfill orders, and manage your account.</p>
+            <p><span class="has-text-weight-bold">3.2. Communication:</span> We use your information to communicate with you regarding your account, orders, inquiries, updates, and promotional offers.</p>
+            <p><span class="has-text-weight-bold">3.3. Improve User Experience:</span> We use usage data and cookies to personalize your experience on our Site, analyze trends, and optimize our content and functionality.</p>
+            <p><span class="has-text-weight-bold">3.4. Marketing and Promotions:</span> With your consent, we may use your information to send you promotional materials, newsletters, and other marketing communications. You can opt-out of receiving marketing communications at any time.</p>
+            <p><span class="has-text-weight-bold">3.5. Legal Compliance and Security:</span> We may use your information to comply with legal obligations, prevent fraud, protect our rights and the rights of others, and ensure the security of our Site and services.</p>
+            <p><span class="has-text-weight-bold">3.6. System Access Control:</span> We reserve the right to disable or restrict your access to our systems, services, or website at any time, without prior notice, for any reason we deem necessary, including but not limited to violations of our Terms of Service, suspected fraudulent activity, or security concerns.</p>
+            <p class="has-text-weight-bold">4. Sharing Your Information</p>
+            <p><span class="has-text-weight-bold">4.1. Third-Party Payment Processors:</span> We share your payment information with third-party payment processors to facilitate transactions.</p>
+            <p><span class="has-text-weight-bold">4.2. Service Providers:</span> We may share your information with third-party service providers who assist us with website hosting, analytics, marketing, customer support, and other services. These providers are contractually obligated to protect your information and use it only for the purposes specified by us.</p>
+            <p><span class="has-text-weight-bold">4.3. Legal Requirements:</span> We may disclose your information if required by law, such as to comply with a subpoena, court order, or other legal process.</p>
+            <p><span class="has-text-weight-bold">4.4. Business Transfers:</span> In the event of a merger, acquisition, or sale of assets, your information may be transferred as part of the transaction.</p>
+            <p class="has-text-weight-bold">5. Data Retention</p>
+            <p>We retain your personal information for as long as necessary to fulfill the purposes for which it was collected, comply with legal obligations, resolve disputes, and enforce our agreements.</p>
+            <p class="has-text-weight-bold">6. Your Rights</p>
+            <p><span class="has-text-weight-bold">6.1. Access and Correction:</span> You have the right to access and correct your personal information.</p>
+            <p><span class="has-text-weight-bold">6.2. Deletion:</span> You have the right to request the deletion of your personal information, subject to legal obligations.</p>
+            <p><span class="has-text-weight-bold">6.3. Opt-Out:</span> You can opt-out of receiving marketing communications at any time.</p>
+            <p><span class="has-text-weight-bold">6.4. Data Portability:</span> You have the right to request a copy of your personal information in a structured, machine-readable format.</p>
+            <p><span class="has-text-weight-bold">6.5. Withdraw Consent:</span> If we rely on your consent to process your information, you have the right to withdraw your consent at any time.</p>
+            <p class="has-text-weight-bold">7. Security</p>
+            <p>We implement reasonable security measures to protect your personal information from unauthorized access, use, or disclosure. However, no method of transmission over the Internet or electronic storage is completely secure, and we cannot guarantee absolute security.</p>
+            <p class="has-text-weight-bold">8. Changes to This Policy</p>
+            <p>We may update this Privacy Policy from time to time. Please review this page periodically for any changes.</p>
+            <p class="has-text-weight-bold">9. Contact Us</p>
+            <p>If you have any questions about this Privacy Policy, please contact us at <span class="has-text-weight-bold">questions@botofthespecter.com</span>.</p>
         </div>
     </div>
 </section>
