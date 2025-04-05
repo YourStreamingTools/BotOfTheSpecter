8c918428f30d72893ef48ad3deb9b251d18ee577
gfaUnDead <contact@gfaundead.stream>
2025-04-05 13:07:01 +1100

    Update ssh.php

--- config/ssh.php
+++ config/ssh.php
@@ -13,4 +13,8 @@ $storage_server_au_east_1_password = '';
 $storage_server_us_west_1_host = '';
 $storage_server_us_west_1_username = '';
 $storage_server_us_west_1_password = '';
+// US East 1
+$storage_server_us_east_1_host = '';
+$storage_server_us_east_1_username = '';
+$storage_server_us_east_1_password = '';
 ?>
