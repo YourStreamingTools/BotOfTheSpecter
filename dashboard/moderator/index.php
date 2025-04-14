85380a5a83922d31c8a964a763152d83f7deca05
gfaUnDead <contact@gfaundead.stream>
2025-04-14 16:34:39 +1000

    Update index.php

--- dashboard/moderator/index.php
+++ dashboard/moderator/index.php
@@ -35,10 +35,47 @@ date_default_timezone_set($timezone);
 
 <div class="container">
   <br>
-  <div class="columns is-desktop is-multiline box-container">
-    Editing User ID: <?php echo $_SESSION['editing_user']; ?><br>
-    Editing User Name: <?php echo $_SESSION['editing_username']; ?><br>
-    Editing User Display Name: <?php echo $_SESSION['editing_display_name']; ?><br>
+  <div class="columns is-desktop">
+    <div class="column is-3">
+      <div class="box">
+        <h3 class="title is-4">Moderator Info</h3>
+        <p><span>Display Name:</span> <?php echo $_SESSION['editing_display_name']; ?></p>
+        <p><span>Channel ID:</span> <?php echo $_SESSION['editing_user']; ?></p>
+        <p><span>Current Date:</span> <?php echo $today->format('F j, Y'); ?></p>
+      </div>
+      <div class="box">
+        <h3 class="title is-4">Quick Actions</h3>
+        <div class="buttons">
+          <a href="manage_custom_commands.php?broadcaster_id=<?php echo $_SESSION['editing_user'];?>" class="button is-primary is-fullwidth mb-2">Manage Custom Commands</a>
+          <a href="timed_messages.php?broadcaster_id=<?php echo $_SESSION['editing_user'];?>" class="button is-info is-fullwidth mb-2">Manage Chat Timers</a>
+          <!--<a href="" class="button is-warning is-fullwidth"></a>-->
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="box">
+        <h3 class="title is-4">Channel Overview</h3>
+        <div class="columns">
+          <?php
+          $commandCount = 0;
+          $timerCount = 0;
+          $activeStatus = "Offline";
+          ?>
+          <div class="column has-text-centered">
+            <p class="heading">Commands</p>
+            <p class="title"><?php echo $commandCount; ?></p>
+          </div>
+          <div class="column has-text-centered">
+            <p class="heading">Timers</p>
+            <p class="title"><?php echo $timerCount; ?></p>
+          </div>
+          <div class="column has-text-centered">
+            <p class="heading">Bot Status</p>
+            <p class="title"><?php echo $activeStatus; ?></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
