a4d91e093e684b303ed86f960583f4f8797f19da
gfaUnDead <contact@gfaundead.stream>
2025-04-14 13:23:09 +1000

    Update modding_access.php

--- dashboard/moderator/modding_access.php
+++ dashboard/moderator/modding_access.php
@@ -3,7 +3,7 @@ require_once "/var/www/config/db_connect.php";
 
 // Check if user is authenticated
 if (!isset($_SESSION['access_token'])) {
-    header('Location: ../../login.php');
+    header('Location: ../../logout.php');
     exit();
 }
 $mod_user = $_SESSION['twitchUserId'];
@@ -12,27 +12,7 @@ $mod_user = $_SESSION['twitchUserId'];
 if (!isset($_GET['broadcaster_id'])) {
     die("Error: Broadcaster ID not provided.");
 }
-
 $broadcasterId = $_GET['broadcaster_id'];
-
-// Fetch broadcaster information from the database
-$query = "SELECT twitch_display_name, profile_image FROM users WHERE twitch_user_id = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("s", $broadcasterId);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows === 0) {
-    die("Error: Broadcaster not found.");
-}
-
-$broadcasterInfo = $result->fetch_assoc();
-
-// Example: Use the fetched data
-$broadcasterName = $broadcasterInfo['twitch_display_name'];
-$broadcasterImage = $broadcasterInfo['profile_image'];
-$twitchDisplayName = $broadcasterName;
-$twitch_profile_image_url = $broadcasterImage;
 
 // Validate the broadcaster_id
 $query = "SELECT COUNT(*) AS valid_count FROM users WHERE twitch_user_id = ?";
@@ -58,4 +38,19 @@ $row = $result->fetch_assoc();
 if ($row['mod_count'] === 0) {
     die("Access Denied: You do not have moderator access for this broadcaster.");
 }
+
+// Fetch broadcaster information from the database
+$query = "SELECT twitch_display_name, profile_image FROM users WHERE twitch_user_id = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("s", $broadcasterId);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows === 0) { die("Error: Broadcaster not found."); }
+$broadcasterInfo = $result->fetch_assoc();
+
+$broadcasterName = $broadcasterInfo['twitch_display_name'];
+$broadcasterImage = $broadcasterInfo['profile_image'];
+$broadcasterUsername = $broadcasterInfo['twitch_username'];
+$twitchDisplayName = $broadcasterName;
+$twitch_profile_image_url = $broadcasterImage;
 ?>
