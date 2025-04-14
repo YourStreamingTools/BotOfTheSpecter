b6878e1ca323b1cbafad04f5c167c226b0fba029
gfaUnDead <contact@gfaundead.stream>
2025-04-14 12:48:17 +1000

    Update sound-alerts.php

--- dashboard/sound-alerts.php
+++ dashboard/sound-alerts.php
@@ -18,6 +18,7 @@ include 'userdata.php';
 include 'bot_control.php';
 include 'user_db.php';
 include 'storage_used.php';
+include "mod_access.php";
 foreach ($profileData as $profile) {
   $timezone = $profile['timezone'];
   $weather = $profile['weather_location'];
