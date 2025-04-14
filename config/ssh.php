10d927f1bdd7fba821c3c5422f48da4ef8b11f0c
gfaUnDead <contact@gfaundead.stream>
2025-04-14 18:41:58 +1000

    Update ssh.php

--- config/ssh.php
+++ config/ssh.php
@@ -4,6 +4,16 @@ $ssh_host = '';
 $ssh_username = '';
 $ssh_password = '';
 
+// API Server Information
+$api_server_host = '';
+$api_server_username = '';
+$api_server_password = '';
+
+// SQL Server Information
+$sql_server_host = '';
+$sql_server_username = '';
+$sql_server_password = '';
+
 // Storage Server Information
 // AU East 1
 $storage_server_au_east_1_host = '';
