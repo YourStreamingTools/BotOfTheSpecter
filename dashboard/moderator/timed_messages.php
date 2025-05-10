855706a06d75a4ae7a4b90c3678a57f0d84f21b0
gfaUnDead <contact@gfaundead.stream>
2025-05-10 11:14:38 +1000

    Update timed_messages.php

--- dashboard/moderator/timed_messages.php
+++ dashboard/moderator/timed_messages.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['access_token'])) {
-    header('Location: login.php');
+    header('Location: ../login.php');
     exit();
 }
 
