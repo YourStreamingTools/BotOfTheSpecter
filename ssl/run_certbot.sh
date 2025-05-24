a986ec326fcefdd7d338717fec2fec113e568dbc
gfaUnDead <contact@gfaundead.stream>
2025-05-24 10:40:01 +1000

    Create run_certbot.sh

---
+++ ssl/run_certbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Load environment variables
+source .certbot.env
+
+# Run Certbot with environment-based values
+certbot certonly --manual \
+  --preferred-challenges=$CHALLENGE_TYPE \
+  --server $ACME_SERVER \
+  --agree-tos \
+  -d $WWW_DOMAIN \
+  -d $WILDCARD_DOMAIN \
+  -d $SUBDOMAIN
