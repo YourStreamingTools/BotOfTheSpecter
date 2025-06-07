cc21dabdd4ddff85873facc595e5137c484315f1
gfaUnDead <contact@gfaundead.stream>
2025-06-07 11:13:14 +1000

    Update todolist.php

--- overlay/todolist.php
+++ overlay/todolist.php
@@ -6,11 +6,12 @@
 <link rel='apple-touch-icon' href='https://yourlistonline.yourcdnonline.com/img/logo.png'>
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
 <?php
-// Include database configuration
+ob_start();
 include '/var/www/config/database.php';
 $primary_db_name = "website";
 $conn = new mysqli($db_servername, $db_username, $db_password, $primary_db_name);
 if ($conn->connect_error) {
+    ob_end_clean();
     die("Connection to primary database failed: " . $conn->connect_error);
 }
 
@@ -27,24 +28,30 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
         $user_id = $user['id'];
         $username = $user['username'];
     } else {
-        echo "</head>";
-        echo "<body>";
-        echo "Invalid API key.<br>Get your API Key from your <a href='https://dashboard.botofthespecter.com/profile.php'>profile</a>.";
-        echo "<p>If you wish to define a working category, please add it like this: <strong>todolist.php?code=API_KEY&category=1</strong></br>";
-        echo "(where ID 1 is called Default defined on the categories page.)</p>";
-        echo "</body>";
-        echo "</html>";
+        ob_end_clean();
+        ?>
+        </head>
+        <body>
+        Invalid API key.<br>Get your API Key from your <a href='https://dashboard.botofthespecter.com/profile.php'>profile</a>.
+        <p>If you wish to define a working category, please add it like this: <strong>todolist.php?code=API_KEY&category=1</strong></br>
+        (where ID 1 is called Default defined on the categories page.)</p>
+        </body>
+        </html>
+        <?php
         exit;
     }
 } else {
-    echo "</head>";
-    echo "<body>";
-    echo "<p>Please provide your API key in the URL like this: <strong>todolist.php?code=API_KEY</strong></p>";
-    echo "<p>Get your API Key from your <a href='https://dashboard.botofthespecter.com/profile.php'>profile</a>.</p>";
-    echo "<p>If you wish to define a working category, please add it like this: <strong>todolist.php?code=API_KEY&category=1</strong></br>";
-    echo "(where ID 1 is called Default defined on the categories page.)</p>";
-    echo "</body>";
-    echo "</html>";
+    ob_end_clean();
+    ?>
+    </head>
+    <body>
+    <p>Please provide your API key in the URL like this: <strong>todolist.php?code=API_KEY</strong></p>
+    <p>Get your API Key from your <a href='https://dashboard.botofthespecter.com/profile.php'>profile</a>.</p>
+    <p>If you wish to define a working category, please add it like this: <strong>todolist.php?code=API_KEY&category=1</strong></br>
+    (where ID 1 is called Default defined on the categories page.)</p>
+    </body>
+    </html>
+    <?php
     exit;
 }
 
@@ -54,6 +61,7 @@ $secondary_db_name = $username;
 // Create a connection to the secondary database using MySQLi
 $user_db = new mysqli($db_servername, $db_username, $db_password, $secondary_db_name);
 if ($user_db->connect_error) {
+    ob_end_clean();
     echo "Connection to secondary database failed: " . $user_db->connect_error;
     exit;
 }
@@ -88,36 +96,30 @@ if ($category_row) {
         $tasks[] = $row;
     }
 } else {
-    echo "</head>";
-    echo "<body>";
-    echo "Invalid category ID.";
-    echo "<br>ID 1 is called Default defined on the categories page, please review this page for a full list of IDs.</p>";
-    echo "</body>";
-    echo "</html>";
+    ob_end_clean();
+    ?>
+    </head>
+    <body>
+    Invalid category ID.
+    <br>ID 1 is called Default defined on the categories page, please review this page for a full list of IDs.</p>
+    </body>
+    </html>
+    <?php
     exit;
 }
 ?>
 <meta http-equiv='refresh' content='10'>
-<style>
-    body {
-        <?php
-        if ($font) {
-            echo "font-family: $font; ";
-        }
-        if ($color) {
-            echo "color: $color;";
+<style>body { <?php
+        if ($font) { echo "font-family: $font; "; }
+        if ($color) { echo "color: $color;";
             if ($shadow && $shadow == 1) {
-                if ($color === 'Black') {
-                    echo "text-shadow: 0px 0px 6px White;";
-                } elseif ($color === 'White') {
-                    echo "text-shadow: 0px 0px 6px Black;";
-                } else {
-                    echo "text-shadow: 0px 0px 6px Black;";
+                if ($color === 'Black') { echo "text-shadow: 0px 0px 6px White;";
+                } elseif ($color === 'White') { echo "text-shadow: 0px 0px 6px Black;";
+                } else { echo "text-shadow: 0px 0px 6px Black;";
                 }
             }
         }
-        ?> }
-</style>
+        ?> }</style>
 </head>
 <body>
 <h1><?php echo htmlspecialchars($category); ?> List:</h1>
