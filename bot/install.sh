915dd491a2871ce6a478e63319df0cb8dc0f2d2b
gfaUnDead <contact@gfaundead.stream>
2025-06-12 22:40:02 +1000

    Update install.sh

--- bot/install.sh
+++ bot/install.sh
@@ -1,6 +1,6 @@
 apt update
 apt-get install apache2 -y
-apt-get install php openssl php-bcmath php-curl php-json php-mbstring php-tokenizer php-xml php-zip php-cli php-common php-mysql php-zip php-curl php-gd php-mbstring php-xml php-bcmath php-fpm php-phpdbg php-cgi libphp-embed libapache2-mod-php -y
+apt-get install php openssl php-bcmath php-sqlite3 php-curl php-json php-mbstring php-mysql php-tokenizer php-xml php-zip php-cli php-common php-mysql php-zip php-curl php-gd php-mbstring php-xml php-bcmath php-fpm php-phpdbg php-cgi libphp-embed libapache2-mod-php -y
 apt install certbot python3-certbot-apache -y
 apt install python3 python-is-python3 python3-pip -y
 pip install -r requirements.txt
