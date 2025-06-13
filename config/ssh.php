ef7d2dc1f32043d1baa122f5f0d7f227fad82dbb
GFAUnDead <contact@gfaundead.stream>
2025-06-13 10:44:36 +1000

    Update ssh.php

--- config/ssh.php
+++ config/ssh.php
@@ -1,30 +1,34 @@
 <?php
+// Main Infomation
+$server_username = '';
+$server_password = '';
+
 // SSH Information
 $ssh_host = '';
 $ssh_username = '';
 $ssh_password = '';
 
+// BOTS Server
+$bots_ssh_host = '';
+$bots_ssh_username = $server_username;
+$bots_ssh_password = $server_password;
+
 // API Server Information
 $api_server_host = '';
-$api_server_username = '';
-$api_server_password = '';
+$api_server_username = $server_username;
+$api_server_password = $server_password;
 
-// SQL Server Information
-$sql_server_host = '';
-$sql_server_username = '';
-$sql_server_password = '';
-
-// Storage Server Information
+// Stream Server Information
 // AU East 1
-$storage_server_au_east_1_host = '';
-$storage_server_au_east_1_username = '';
-$storage_server_au_east_1_password = '';
-// US West 1
-$storage_server_us_west_1_host = '';
-$storage_server_us_west_1_username = '';
-$storage_server_us_west_1_password = '';
+$stream_au_east_1_host = '';
+$stream_au_east_1_username = $server_username;
+$stream_au_east_1_password = $server_password;
 // US East 1
-$storage_server_us_east_1_host = '';
-$storage_server_us_east_1_username = '';
-$storage_server_us_east_1_password = '';
+$stream_us_east_1_host = '';
+$stream_us_east_1_username = $server_username;
+$stream_us_east_1_password = $server_password;
+// US West 1
+$stream_us_west_1_host = '';
+$stream_us_west_1_username = $server_username;
+$stream_us_west_1_password = $server_password;
 ?>
