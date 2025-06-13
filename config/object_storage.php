23aff04a319bbb804a4a749f5795751591694d64
GFAUnDead <contact@gfaundead.stream>
2025-06-13 10:45:12 +1000

    Update object_storage.php

--- config/object_storage.php
+++ config/object_storage.php
@@ -2,4 +2,14 @@
 $access_key = "";
 $secret_key = "";
 $bucket_url = "";
+
+// NEW SERVERS
+// AU
+$au_s3_access_key = "";
+$au_s3_secret_key = "";
+$au_s3_bucket_url = "";
+// US
+$us_s3_access_key = "";
+$us_s3_secret_key = "";
+$us_s3_bucket_url = "";
 ?>
