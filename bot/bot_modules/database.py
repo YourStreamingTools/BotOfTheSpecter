ed8dbdd8fb394df592e03f60cb30e328c26233f0
GFAUnDead <contact@gfaundead.stream>
2025-06-18 12:19:50 +1000

    Update database.py

--- bot/bot_modules/database.py
+++ bot/bot_modules/database.py
@@ -14,3 +14,27 @@ async def get_mysql_connection(CHANNEL_NAME):
         password=SQL_PASSWORD,
         db=CHANNEL_NAME
     )
+
+async def access_website_database():
+    return await aiomysql.connect(
+        host=SQL_HOST,
+        user=SQL_USER,
+        password=SQL_PASSWORD,
+        db="website",
+    )
+
+async def get_spam_patterns():
+    pattern_db = await aiomysql.connect(
+        host=SQL_HOST,
+        user=SQL_USER,
+        password=SQL_PASSWORD,
+        db="spam_pattern",
+    )
+    async with pattern_db.cursor(aiomysql.DictCursor) as cursor:
+        await cursor.execute("SELECT spam_pattern FROM spam_patterns")
+        results = await cursor.fetchall()
+    pattern_db.close()
+    # Compile the regular expressions
+    import re
+    compiled_patterns = [re.compile(row["spam_pattern"], re.IGNORECASE) for row in results if row["spam_pattern"]]
+    return compiled_patterns
