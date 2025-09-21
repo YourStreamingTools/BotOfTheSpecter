06c7ad0186465769e6ce98abaf26778daf5898d0
gfaUnDead <contact@gfaundead.stream>
2025-09-21 23:35:16 +1000

    Dashboard version to 2.1.4 and update CSS root color
    Updated the dashboard version in config/main.php to 2.1.4. Added a global color style to :root in custom.css, setting the default text color to white.

--- config/main.php
+++ config/main.php
@@ -1,5 +1,5 @@
 <?php
-$dashboardVersion = '2.1.3';
+$dashboardVersion = '2.1.4';
 $maintenanceMode = false; // Set to true to enable maintenance mode
 return [
     'dashboardVersion' => $dashboardVersion,
