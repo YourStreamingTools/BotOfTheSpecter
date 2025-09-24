f72f22cfa3e89cf1124ce041dcc92d6cd22022ab
gfaUnDead <contact@gfaundead.stream>
2025-09-24 18:04:02 +1000

    Bump dashboard version to 2.2.0
    Updated the $dashboardVersion variable in config/main.php from 2.1.5 to 2.2.0 to reflect the new release.

--- config/main.php
+++ config/main.php
@@ -1,5 +1,5 @@
 <?php
-$dashboardVersion = '2.1.5';
+$dashboardVersion = '2.2.0';
 $maintenanceMode = false; // Set to true to enable maintenance mode
 return [
     'dashboardVersion' => $dashboardVersion,
