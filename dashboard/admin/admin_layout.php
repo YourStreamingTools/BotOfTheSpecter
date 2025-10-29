868df923e4a3fb0d94ff6c7f24c6c2c3d4d490a9
gfaUnDead <contact@gfaundead.stream>
2025-10-29 21:22:37 +1100

    Simplify uuidv4 function for cache busting
    Replaces the UUID v4 generation logic with a simpler approach that returns an 8-character hex string using random_bytes. This change streamlines cache busting and reduces code complexity.

--- dashboard/admin/admin_layout.php
+++ dashboard/admin/admin_layout.php
@@ -11,17 +11,7 @@ $maintenanceMode = $config['maintenanceMode'];
 
 // Function to generate a UUID v4 for cache busting
 function uuidv4() {
-    $data = random_bytes(16);
-    $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
-    $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
-    $hex = bin2hex($data);
-    return sprintf('%s-%s-%s-%s-%s',
-        substr($hex, 0, 8),
-        substr($hex, 8, 4),
-        substr($hex, 12, 4),
-        substr($hex, 16, 4),
-        substr($hex, 20, 12)
-    );
+    return bin2hex(random_bytes(4));
 }
 
 if (!isset($pageTitle)) $pageTitle = "BotOfTheSpecter Admin";
