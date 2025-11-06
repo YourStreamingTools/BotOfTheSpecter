cb79daebd1cea576683ebec94ac4307e8ac9d0bc
GFAUnDead <contact@gfaundead.stream>
2025-11-07 09:56:38 +1100

    Remove error display settings from admin index
    Deleted PHP error display and reporting configuration from dashboard/admin/index.php to prevent errors from being shown in production.

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
 $userLanguage = isset($_SESSION['language']) ? $_SESSION['language'] : (isset($user['language']) ? $user['language'] : 'EN');
 include_once __DIR__ . '/../lang/i18n.php';
