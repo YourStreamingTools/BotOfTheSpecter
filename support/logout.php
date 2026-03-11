730ca7b2e3aa21f955106a42e2d05653f2cee039
gfaUnDead <contact@gfaundead.stream>
2026-03-11 17:32:50 +1100

    Redirect logout to support subdomain
    Change the post-logout redirect in support/logout.php from https://botofthespecter.com to https://support.botofthespecter.com so users are returned to the support site after their session is destroyed.

--- support/logout.php
+++ support/logout.php
@@ -11,5 +11,5 @@ if (ini_get('session.use_cookies')) {
     );
 }
 session_destroy();
-header('Location: https://botofthespecter.com');
+header('Location: https://support.botofthespecter.com');
 exit;
